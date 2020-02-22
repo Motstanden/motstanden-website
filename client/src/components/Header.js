cf46b143efceb0ae6cc7dd4f5e59c9eec234b010
raflemakt <raflemakt@hotmail.com>
2020-02-22 17:18:59 +0100

    Working on login button for the header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -3,6 +3,7 @@ import styles from "./header.module.css"
 import {Link} from "react-router-dom"
 
 import Sidebar from "./sidebar/Sidebar.js"
+import LogInPopup from "./popups/LogInPopup.js"
 
 class Header extends React.Component {
     render() {    
@@ -14,7 +15,9 @@ class Header extends React.Component {
                         <Link to="/"  >
                             Studentorchesteret den Ohmske <span className={styles.green} >Motstanden</span>
                         </Link>
+                        
                     </h1>
+                    <LogInPopup className={styles.loginButton}/>
                     <Sidebar/> 
                 </div>
             </header>
