1a8b58d87817856288c767dcee7a4f5e710213b0
Torstein Wilhelmsen Sandvik <torsteinws@gmail.com>
2020-02-23 03:04:26 +0100

    Began creating a list component that will be used for lyrics and sheet arcive

--- client/src/components/Lyrics.js
+++ client/src/components/Lyrics.js
@@ -1,15 +1,37 @@
 import React from "react"
 import styles from "./Lyrics.module.css"
+import List from "./List.js"
+
 
 import LyricsData from "./LyricsData.js"
+import axios from "axios"
 
 class Lyrics extends React.Component {
+
+    constructor(props){
+        super(props)
+
+        this.state = {
+            lyricsTitleArray: []
+        }
+
+        axios.get("/api/song_lyric")
+            .then( (res) => {
+                
+                this.setState({
+                    lyricsTitleArray: res.data.lyricsArray
+                })
+            })
+        
+    }
+
+
     render(){
         return(
             <main className={styles.main}>
                 <h1>Studenttraller</h1>
+                <List itemSource={this.state.lyricsTitleArray} />
                 {/* <LyricsData/> */}
-                
             </main>
         )
     }
