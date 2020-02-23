2f10eb62bc2ec0ef8eee3448d7dbc5ea870e062e
Torstein Wilhelmsen Sandvik <torsteinws@gmail.com>
2020-02-23 23:49:06 +0100

    test

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ const app = express()
 app.use(helmet())
 
 // Alows us to make request from localhost:3000 and whatever domain the server is running on
-const whiteList = [ "http://localhost:3000"]
+const whiteList = [ "https://localhost:3000", "http://localhost:3000"]
 const corsOptions = {
     origin: (origin, callback) => {
         if(whiteList.indexOf(origin) !== -1 || !origin){
