f7029b91de78b23e7787a08be699fdd9d7052470
Torstein Wilhelmsen Sandvik <MotstandenOkononomi>
2020-02-27 06:54:41 +0100

    A few minor changes

--- client/src/components/pages/MusicArcivePage.js
+++ client/src/components/pages/MusicArcivePage.js
@@ -48,17 +48,22 @@ class MusicArcivePage extends React.Component {
     render(){
 
         let error = null
+        let introduction = null
         if (this.state.songArray.length === 0){
             error = (
                 <p className={styles.errorMessage}>
                     Du må være logget inn for å se notearkivet.
                 </p>)
         }
+        else {
+            introduction = (<p>Obs: Noen av filene har en stor størrelse. Det kan derfor ta et par sekunder før de blir lastet inn i nettleseren.</p>)
+        }
 
         return (
             <main className={styles.main}>
                 <h1>Notearkiv</h1>
                 {error}
+                {introduction}
                 <ul className={styles.ul}>
                 {
                     this.state.songArray.map( (song) => (
