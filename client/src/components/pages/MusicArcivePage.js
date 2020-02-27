cc7e59f60ae12b92bc0252d76a11fbf1cb78e330
Torstein Wilhelmsen Sandvik <MotstandenOkononomi>
2020-02-27 03:50:10 +0100

    The program will now fetch pdf files from the database

--- client/src/components/pages/MusicArcivePage.js
+++ client/src/components/pages/MusicArcivePage.js
@@ -1,14 +1,78 @@
 import React from "react"
 import styles from "./MusicArcivePage.module.css"
-
-
+import axios from "axios"
 
 class MusicArcivePage extends React.Component {
+
+    constructor(props){
+        super(props)
+        this.state = {
+            songArray: []
+        }
+
+        const accessToken = localStorage.getItem("accessToken")
+
+        axios.get("/api/sheet_arcive_song", {
+            headers: {'Authorization': "Bearer " +  accessToken}
+            })
+            .then( (res) => {
+                console.log(res.data)
+                this.setState({
+                    songArray: res.data
+                })
+            })
+    }
+
+    onSongTitleClick = (id, title) => {
+        
+        const accessToken = localStorage.getItem("accessToken")
+
+        axios.get("/api/sheet_arcive_file", {
+            headers: {'Authorization': "Bearer " +  accessToken},
+            responseType: 'blob',
+            params: {
+                song_id: id
+            }})
+            .then( res => {
+                console.log(res)
+                const url = window.URL.createObjectURL(new Blob([res.data]))
+                const link = document.createElement('a')
+                link.href = url
+                link.setAttribute('download', title + ".pdf")
+                document.body.appendChild(link)
+                link.click()
+            })
+            .catch( err => console.log(err))
+    }
+
     render(){
+
+        let error = null
+        if (this.state.songArray.length === 0){
+            error = (
+                <p className={styles.errorMessage}>
+                    Du må være logget inn for å se notearkivet.
+                </p>)
+        }
+
         return (
             <main className={styles.main}>
                 <h1>Notearkiv</h1>
-                <p className={styles.errorMessage}>Du må være logget inn for å se notearkivet.</p>
+                {error}
+                <ul className={styles.ul}>
+                {
+                    this.state.songArray.map( (song) => (
+                        <li>
+                            <button 
+                                onClick ={ (event) => this.onSongTitleClick(song.song_id, song.title, event)}
+                                className = {styles.buttonStyle}
+                                >
+                                {song.title}
+                            </button>
+                        </li>
+                    ))
+                }
+                </ul>
             </main>
         )
     }
