05870927a61fc6851641c7f207706ebea946aad0
Torstein Wilhelmsen Sandvik <torstein.sandvik@nordicsemi.no>
2020-08-10 17:15:43 +0200

    test

--- server.js
+++ server.js
@@ -198,7 +198,7 @@ app.post("/api/repository-update",
     (req, res) => {
         console.log("Update server here")
 
-        // test num: 3
+        // test num: 4
 })
 
 // Allows us to use files from the paths: './' and './client/.build'
