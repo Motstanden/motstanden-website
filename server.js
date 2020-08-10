29f6231cf70776aa6fcf07690531a2eaae1f1f65
Torstein Wilhelmsen Sandvik <torstein.sandvik@nordicsemi.no>
2020-08-10 15:57:24 +0200

    test

--- server.js
+++ server.js
@@ -210,7 +210,7 @@ app.post("/api/repository-update", (req, res) => {
     console.log("body:", req.body)
 
     if (isAllowed && isMaster) {
-      // Test num: 2
+      // Test num: 3
     }
 
 })
