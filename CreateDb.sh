c1b9813b4d8f4c965e3ade26ee1094c80592afcf
Torstein Wilhelmsen Sandvik <torsteinws@gmail.com>
2021-05-01 23:20:06 +0200

    Create and use db table for documents

--- CreateDb.sh
+++ CreateDb.sh
@@ -5,3 +5,5 @@ echo "Inserting quotes"
 sqlite3 motstanden.db < database/InsertQuotes.sql;
 echo "Inserting users"
 sqlite3 motstanden.db < database/InsertUser.sql;
+echo "Inserting documents"
+sqlite3 motstanden.db < database/InsertDocuments.sql;
---
+++ database/InsertDocuments.sql
@@ -0,0 +1,3 @@
+INSERT INTO document(title, root_path, filename) VALUES
+('Bulleltin 2019-2020', 'files/', 'dokumenter/bulleltin-2019-2020.pdf'),
+('Motstandens Statutter', 'files/', 'dokumenter/motstandens-statutter.pdf');
