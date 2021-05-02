db2c47196bab3d3b683d3980f598c7aa34262c25
Torstein Wilhelmsen Sandvik <torsteinws@gmail.com>
2021-05-02 16:42:42 +0200

    Add is_public column to document, sheet_archive and song_lyric.  - Auto generate url from is_public column.

--- database/CreateDb.sql
+++ database/CreateDb.sql
@@ -33,7 +33,8 @@ CREATE TABLE document (
     title TEXT NOT NULL,
     root_path TEXT,
     filename TEXT NOT NULL,
-    url TEXT GENERATED ALWAYS AS ('/api/' || filename) STORED
+    is_public BOOLEAN NOT NULL DEFAULT 0,
+    url TEXT GENERATED ALWAYS AS (CASE is_public WHEN 1 THEN '/api/files/public/' || filename ELSE '/api/files/private/' || filename END) VIRTUAL
 );
 
 -- TODO: Rewrite this
@@ -41,8 +42,9 @@ CREATE TABLE sheet_archive (
     sheet_archive_id INTEGER PRIMARY KEY NOT NULL,
     title TEXT NOT NULL,
     root_path TEXT,
+    is_public BOOLEAN NOT NULL DEFAULT 0,
     filename TEXT NOT NULL,
-    url TEXT GENERATED ALWAYS AS ('/api/' || filename) STORED
+    url TEXT GENERATED ALWAYS AS (CASE is_public WHEN 1 THEN '/api/files/public/' || filename ELSE '/api/files/private/' || filename END) VIRTUAL
 );
 
 CREATE TABLE song_lyric (
@@ -50,7 +52,8 @@ CREATE TABLE song_lyric (
     title TEXT NOT NULL,
     root_path TEXT NOT NULL,
     filename TEXT NOT NULL,
-    url TEXT GENERATED ALWAYS AS ('/api/' || filename) STORED,
+    is_public BOOLEAN NOT NULL DEFAULT 0,
+    url TEXT GENERATED ALWAYS AS (CASE is_public WHEN 1 THEN '/api/files/public/' || filename ELSE '/api/files/private/' || filename END) VIRTUAL,
     song_melody TEXT,
     song_text_origin TEXT,
     song_description TEXT
