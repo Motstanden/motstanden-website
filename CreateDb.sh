29e3c4fc072dcfbf55e1b92b3cb7796e0fb7b547
Torstein Wilhelmsen Sandvik <torsteinws@gmail.com>
2021-05-02 18:43:44 +0200

    Add ; after echo

--- CreateDb.sh
+++ CreateDb.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 rm -fv motstanden.db;
-echo "Creating new db"
+echo "Creating new db";
 sqlite3 motstanden.db < database/CreateDb.sql;
-echo "Inserting quotes"
+echo "Inserting quotes";
 sqlite3 motstanden.db < database/InsertQuotes.sql;
-echo "Inserting users"
+echo "Inserting users";
 sqlite3 motstanden.db < database/InsertUser.sql;
-echo "Inserting documents"
+echo "Inserting documents";
 sqlite3 motstanden.db < database/InsertDocuments.sql;
-echo "Inserting sheet music"
+echo "Inserting sheet music";
 sqlite3 motstanden.db < database/InsertSheets.sql;
-echo "Inserting song lyrics"
+echo "Inserting song lyrics";
 sqlite3 motstanden.db < database/InsertLyrics.sql;
