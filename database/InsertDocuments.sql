2de3829366532a4fcd2607c1bbe5052071267e8b
Torstein Wilhelmsen Sandvik <torsteinws@gmail.com>
2021-05-02 13:46:12 +0200

    Separate files into a public and a private directory

--- database/InsertDocuments.sql
+++ database/InsertDocuments.sql
@@ -1,3 +1,3 @@
 INSERT INTO document(title, root_path, filename) VALUES
-('Bulleltin 2019-2020', 'files/', 'dokumenter/bulleltin-2019-2020.pdf'),
-('Motstandens Statutter', 'files/', 'dokumenter/motstandens-statutter.pdf');
+('Bulleltin 2019-2020',     'files/private/', 'dokumenter/bulleltin-2019-2020.pdf'),
+('Motstandens Statutter',   'files/private/', 'dokumenter/motstandens-statutter.pdf');
