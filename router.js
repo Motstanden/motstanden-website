9159c14ce2b7104c789ede439ba4021242f18d35
Torstein Wilhelmsen Sandvik <torsteinws@gmail.com>
2021-09-03 19:42:40 +0200

    Move login to separate file

--- router.js
+++ router.js
@@ -1,9 +1,11 @@
 const router = require("express").Router()
+const login = require("./routes/login")
 const songLyric = require("./routes/songLyric")
 const documents = require("./routes/documents")
 const quotes = require("./routes/quotes")
 const debugRouter = require("./routes/debug/debugRouter")
 
+router.use(login)
 router.use(songLyric)
 router.use(documents)
 router.use(quotes)
