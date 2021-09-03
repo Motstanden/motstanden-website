b7b014422dd198d4c6e6b863908d2c070d893b66
Torstein Wilhelmsen Sandvik <torsteinws@gmail.com>
2021-09-03 17:59:17 +0200

    Move documents route to new file

--- router.js
+++ router.js
@@ -1,6 +1,8 @@
 const router = require("express").Router()
 const songLyric = require("./routes/songLyric")
+const documents = require("./routes/documents")
 
 router.use(songLyric)
+router.use(documents)
 
 module.exports = router;
