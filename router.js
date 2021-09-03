3f738e81eb586abd72c41c001a1f429123d6b82f
Torstein Wilhelmsen Sandvik <torsteinws@gmail.com>
2021-09-03 18:40:28 +0200

    Move quote api to separate file

--- router.js
+++ router.js
@@ -1,8 +1,10 @@
 const router = require("express").Router()
 const songLyric = require("./routes/songLyric")
 const documents = require("./routes/documents")
+const quotes = require("./routes/quotes")
 
 router.use(songLyric)
 router.use(documents)
+router.use(quotes)
 
 module.exports = router;
