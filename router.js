e6177423e8da6da5c554a788907bee29546bce86
Torstein Wilhelmsen Sandvik <torsteinws@gmail.com>
2021-09-03 19:50:53 +0200

    Move sheetArchive route to separate file

--- router.js
+++ router.js
@@ -3,12 +3,14 @@ const login = require("./routes/login")
 const songLyric = require("./routes/songLyric")
 const documents = require("./routes/documents")
 const quotes = require("./routes/quotes")
+const sheetArchive = require("./routes/sheetArchive")
 const debugRouter = require("./routes/debug/debugRouter")
 
 router.use(login)
 router.use(songLyric)
 router.use(documents)
 router.use(quotes)
+router.use(sheetArchive)
 router.use("/debug", debugRouter)
 
 module.exports = router;
