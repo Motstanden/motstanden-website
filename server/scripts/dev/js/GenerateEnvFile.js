5da5e4f489bcad629610ebe44f32c45e1e1751b4
Torstein Wilhelmsen Sandvik <torsteinws@gmail.com>
2021-09-04 16:21:12 +0200

    Create script that creates dev .env files

---
+++ server/scripts/dev/js/GenerateEnvFile.js
@@ -0,0 +1,46 @@
+// ******************************************************************
+//  Summary:
+//      This scripts generate a .env file that can be used in a dev environment. 
+//      The .env file is similar to the secret .env file on the server
+//
+//  Usage:
+//      1.  Run from terminal: node GenerateEnvFile
+//      2.  Copy .env file to server folder 
+//
+// ******************************************************************
+
+const fs = require('fs')
+const path = require("path")
+const { randomBytes } = require('crypto');
+
+let envObjects = []
+
+// Dev Environment
+envObjects.push({
+    name: "IS_DEV_ENV",
+    value: true
+})
+
+// Access token
+const accessToken = randomBytes(256)
+envObjects.push({
+    name: "ACCESS_TOKEN_SECRET",
+    value: accessToken.toString('hex')
+})
+
+// Refresh token
+const refressToken = randomBytes(256)
+envObjects.push({
+    name: "REFRESH_TOKEN_SECRET",
+    value: refressToken.toString('hex')
+})
+
+// Build string with the env data
+let fileString = ""
+envObjects.forEach(item => {
+    fileString += `${ item.name }=${ item.value }\n`
+});
+
+// Write data string to file
+const filename = path.join(__dirname, ".env")
+fs.writeFileSync(filename, fileString)
