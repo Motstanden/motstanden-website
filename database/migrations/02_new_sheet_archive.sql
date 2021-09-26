7f3a49d2fa1e20004da65741b898ecff7ecc6abe
Torstein Wilhelmsen Sandvik <torsteinws@gmail.com>
2021-09-26 13:50:31 +0200

    Move 5- and 7 part system data to separate file

--- database/migrations/02_new_sheet_archive.sql
+++ database/migrations/02_new_sheet_archive.sql
@@ -203,21 +203,6 @@ CREATE TABLE five_part_system(
         ON DELETE RESTRICT
 );
 
--- Pikkolofløyte: Part 1-3
-INSERT INTO five_part_system SELECT instrument_id, 1 FROM instrument WHERE instrument = 'Pikkolofløyte';
-INSERT INTO five_part_system SELECT instrument_id, 2 FROM instrument WHERE instrument = 'Pikkolofløyte';
-INSERT INTO five_part_system SELECT instrument_id, 3 FROM instrument WHERE instrument = 'Pikkolofløyte';
-
--- Tverrfløyte: Part 2-4
-INSERT INTO five_part_system SELECT instrument_id, 2 FROM instrument WHERE instrument = 'Tverrfløyte';
-INSERT INTO five_part_system SELECT instrument_id, 3 FROM instrument WHERE instrument = 'Tverrfløyte';
-INSERT INTO five_part_system SELECT instrument_id, 4 FROM instrument WHERE instrument = 'Tverrfløyte';
-
--- Obo: Part 4-5
-INSERT INTO five_part_system SELECT instrument_id, 4 FROM instrument WHERE instrument = 'Obo';
-INSERT INTO five_part_system SELECT instrument_id, 5 FROM instrument WHERE instrument = 'Obo';
-
-
 CREATE TABLE seven_part_system(
     instrument_id INTEGER NOT NULL,
     part_number INTEGER NOT NULL CHECK (part_number >= 1 AND part_number <= 7),
@@ -227,20 +212,6 @@ CREATE TABLE seven_part_system(
         ON UPDATE CASCADE
         ON DELETE RESTRICT
 );
-
--- Pikkolofløyte: Part 1-3
-INSERT INTO seven_part_system SELECT instrument_id, 1 FROM instrument WHERE instrument = 'Pikkolofløyte';
-INSERT INTO seven_part_system SELECT instrument_id, 2 FROM instrument WHERE instrument = 'Pikkolofløyte';
-INSERT INTO seven_part_system SELECT instrument_id, 3 FROM instrument WHERE instrument = 'Pikkolofløyte';
-
--- Tverrfløyte: Part 3-5
-INSERT INTO seven_part_system SELECT instrument_id, 3 FROM instrument WHERE instrument = 'Tverrfløyte';
-INSERT INTO seven_part_system SELECT instrument_id, 4 FROM instrument WHERE instrument = 'Tverrfløyte';
-INSERT INTO seven_part_system SELECT instrument_id, 5 FROM instrument WHERE instrument = 'Tverrfløyte';
-
--- Obo: Part 6-7
-INSERT INTO seven_part_system SELECT instrument_id, 6 FROM instrument WHERE instrument = 'Obo';
-INSERT INTO seven_part_system SELECT instrument_id, 7 FROM instrument WHERE instrument = 'Obo';
 
 -- ****************** WIP *************************
 
---
+++ database/migrations/03_insert_sheet_archive_data.sql
@@ -0,0 +1,36 @@
+-- ::::::::::::::::::::::::::::::::::::::::::::::::
+--              Five part system
+-- ::::::::::::::::::::::::::::::::::::::::::::::::
+
+-- Pikkolofløyte: Part 1-3
+INSERT INTO five_part_system SELECT instrument_id, 1 FROM instrument WHERE instrument = 'Pikkolofløyte';
+INSERT INTO five_part_system SELECT instrument_id, 2 FROM instrument WHERE instrument = 'Pikkolofløyte';
+INSERT INTO five_part_system SELECT instrument_id, 3 FROM instrument WHERE instrument = 'Pikkolofløyte';
+
+-- Tverrfløyte: Part 2-4
+INSERT INTO five_part_system SELECT instrument_id, 2 FROM instrument WHERE instrument = 'Tverrfløyte';
+INSERT INTO five_part_system SELECT instrument_id, 3 FROM instrument WHERE instrument = 'Tverrfløyte';
+INSERT INTO five_part_system SELECT instrument_id, 4 FROM instrument WHERE instrument = 'Tverrfløyte';
+
+-- Obo: Part 4-5
+INSERT INTO five_part_system SELECT instrument_id, 4 FROM instrument WHERE instrument = 'Obo';
+INSERT INTO five_part_system SELECT instrument_id, 5 FROM instrument WHERE instrument = 'Obo';
+
+
+-- ::::::::::::::::::::::::::::::::::::::::::::::::
+--              Seven part system
+-- ::::::::::::::::::::::::::::::::::::::::::::::::
+
+-- Pikkolofløyte: Part 1-3
+INSERT INTO seven_part_system SELECT instrument_id, 1 FROM instrument WHERE instrument = 'Pikkolofløyte';
+INSERT INTO seven_part_system SELECT instrument_id, 2 FROM instrument WHERE instrument = 'Pikkolofløyte';
+INSERT INTO seven_part_system SELECT instrument_id, 3 FROM instrument WHERE instrument = 'Pikkolofløyte';
+
+-- Tverrfløyte: Part 3-5
+INSERT INTO seven_part_system SELECT instrument_id, 3 FROM instrument WHERE instrument = 'Tverrfløyte';
+INSERT INTO seven_part_system SELECT instrument_id, 4 FROM instrument WHERE instrument = 'Tverrfløyte';
+INSERT INTO seven_part_system SELECT instrument_id, 5 FROM instrument WHERE instrument = 'Tverrfløyte';
+
+-- Obo: Part 6-7
+INSERT INTO seven_part_system SELECT instrument_id, 6 FROM instrument WHERE instrument = 'Obo';
+INSERT INTO seven_part_system SELECT instrument_id, 7 FROM instrument WHERE instrument = 'Obo';
