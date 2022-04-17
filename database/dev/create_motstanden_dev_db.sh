32c054983d417d5773482921b9dff1dabd0747eb
Torstein Wilhelmsen Sandvik <torsteinws@gmail.com>
2022-04-17 21:46:54 +0200

    Refactor/restructure bash scripts that creates dev db

---
+++ database/dev/create_motstanden_dev_db.sh
@@ -0,0 +1,23 @@
+
+#!/bin/bash
+cd ..
+
+rm -fv motstanden_dev.db;
+
+# First create the current database schema
+echo "Running: 01_initial.sql";
+sqlite3 motstanden_dev.db < migrations/motstanden_db/01_initial.sql;
+
+# Insert data that is representative for the current data in the database
+pushd dev/data/sql-table-inserts > /dev/null
+sh InsertData.sh 
+popd > /dev/null
+
+# Refactor current tables
+echo "Running: 02_refactor_db.sql"
+sqlite3 motstanden_dev.db < migrations/motstanden_db/02_refactor_db.sql;
+
+echo "Dumping schema -> motstanden_schema.sql"
+sqlite3 motstanden_dev.db .schema > motstanden_schema.sql 
+
+cd dev
