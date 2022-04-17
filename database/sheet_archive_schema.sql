21ffe2f28c2a0a4d404e8f24f19e798555ddb5d0
Torstein Wilhelmsen Sandvik <torsteinws@gmail.com>
2022-04-17 19:32:21 +0200

    Break out sheet_archive to separate sqlite db

--- database/sheet_archive_schema.sql
+++ database/sheet_archive_schema.sql
@@ -1,43 +1,7 @@
-CREATE TABLE ping(
-    ping TEXT
-);
-CREATE TABLE quote (
-    quote_id INTEGER PRIMARY KEY NOT NULL,
-    utterer TEXT NOT NULL,
-    quote TEXT NOT NULL 
-);
-CREATE TABLE user_account (
-    user_account_id INTEGER PRIMARY KEY NOT NULL,
-    username TEXT NOT NULL,
-    password TEXT NOT NULL
-);
-CREATE TABLE sheet_archive (
-    sheet_archive_id INTEGER PRIMARY KEY NOT NULL,
-    title TEXT NOT NULL,
-    root_path TEXT,
-    is_public BOOLEAN NOT NULL DEFAULT 0,
-    filename TEXT NOT NULL,
-    full_filename TEXT GENERATED ALWAYS AS (root_path || filename) VIRTUAL,
-    url TEXT GENERATED ALWAYS AS (CASE is_public WHEN 1 THEN '/api/files/public/' || filename ELSE '/api/files/private/' || filename END) VIRTUAL
-);
 CREATE TABLE version (
     version_id INTEGER PRIMARY KEY NOT NULL,
     migration TEXT,
     create_time TEXT NOT NULL DEFAULT CURRENT_TIMESTAMP
-);
-CREATE TABLE IF NOT EXISTS "document"(
-    document_id INTEGER PRIMARY KEY NOT NULL,
-    title TEXT NOT NULL,
-    filename TEXT NOT NULL CHECK(like('files/public/dokumenter/%_._%', filename) OR like('files/private/dokumenter/%_._%', filename)),
-    is_public BOOLEAN NOT NULL GENERATED ALWAYS AS (iif(like('files/public/%', filename), 1, 0)) STORED
-);
-CREATE TABLE IF NOT EXISTS "song_lyric"(
-    song_lyric_id INTEGER PRIMARY KEY NOT NULL,
-    title TEXT NOT NULL,
-    filename TEXT NOT NULL CHECK(like('files/public/studenttraller/%_.html', filename)),
-    song_melody TEXT,
-    song_text_origin TEXT,
-    song_description TEXT
 );
 CREATE TABLE tone(
     tone_id INTEGER PRIMARY KEY NOT NULL,
