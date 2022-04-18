d513ac5c7108dd8ff6b83ec0befc7005fa590118
Torstein Wilhelmsen Sandvik <torsteinws@gmail.com>
2022-04-18 22:45:36 +0200

    Remove old sheet archive

--- database/motstanden_schema.sql
+++ database/motstanden_schema.sql
@@ -11,15 +11,6 @@ CREATE TABLE user_account (
     username TEXT NOT NULL,
     password TEXT NOT NULL
 );
-CREATE TABLE sheet_archive (
-    sheet_archive_id INTEGER PRIMARY KEY NOT NULL,
-    title TEXT NOT NULL,
-    root_path TEXT,
-    is_public BOOLEAN NOT NULL DEFAULT 0,
-    filename TEXT NOT NULL,
-    full_filename TEXT GENERATED ALWAYS AS (root_path || filename) VIRTUAL,
-    url TEXT GENERATED ALWAYS AS (CASE is_public WHEN 1 THEN '/api/files/public/' || filename ELSE '/api/files/private/' || filename END) VIRTUAL
-);
 CREATE TABLE version (
     version_id INTEGER PRIMARY KEY NOT NULL,
     migration TEXT,
