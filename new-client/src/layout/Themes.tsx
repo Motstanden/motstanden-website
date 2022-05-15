6a009f2b15df80e94baf6984f7970087f3607c39
Torstein Wilhelmsen Sandvik <torsteinws@gmail.com>
2022-05-15 03:38:15 +0200

    Check os for what theme to use. Save prefered them in local storage

--- new-client/src/layout/Themes.tsx
+++ new-client/src/layout/Themes.tsx
@@ -24,28 +24,43 @@ interface AppThemeContextType extends AppThemeType {
     changeTheme: (newThemeName: ThemeNameType, callback?: VoidFunction ) => void
 }
 
+
 export const AppThemeContext = createContext<AppThemeContextType>(null!)
 
 export function useAppTheme(){
     return useContext(AppThemeContext)
 }
 
+const OsPreferDarkMode = () :boolean => 
+    window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
+
+const getDefaultThemeName = () => {
+    let themeName = localStorage.getItem("AppTheme") as ThemeNameType | null
+    themeName ??= OsPreferDarkMode() ? "dark" : "light"
+    return themeName
+}
+
+const setDefaultThemeName = (name: ThemeNameType) => {
+    localStorage.setItem("AppTheme", name)
+}
+
+const getThemeFromName = (name: ThemeNameType): AppThemeType => {
+    return name === "light"
+    ? {theme: lightTheme, name: "light"}
+    : {theme: darkTheme, name: "dark"}
+}
 
 export function AppThemeProvider({ children }: {children: React.ReactNode} ) {
-
-    let defaultTheme: AppThemeType = {theme: darkTheme, name: "dark"}
-
+    let defaultTheme = getThemeFromName(getDefaultThemeName())
     const [themeInfo, setTheme] = useState<AppThemeType>(defaultTheme)
 
     const changeTheme = (newThemeName: ThemeNameType, callback?: VoidFunction) => {
         if(newThemeName === themeInfo.name) return;
 
-        let newThemeInfo: AppThemeType = newThemeName === "light"
-            ? {theme: lightTheme, name: "light"}
-            : {theme: darkTheme, name: "dark"}
+        let newThemeInfo: AppThemeType = getThemeFromName(newThemeName)
         
         setTheme(newThemeInfo)
-
+        setDefaultThemeName(newThemeInfo.name)
         if(callback) callback();
     }
 
