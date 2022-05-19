8a6a417498ebc4ce680e18c843eb7b6c936ea755
Torstein Wilhelmsen Sandvik <torsteinws@gmail.com>
2022-05-19 20:20:39 +0200

    Remove old unused code

--- new-client/src/layout/PageLayout2.tsx
+++ new-client/src/layout/PageLayout2.tsx
@@ -1,66 +1,20 @@
 import React from "react"
-import { Link, Outlet, useNavigate } from "react-router-dom"
-import { useAuth } from "../contextProviders/Authentication"
+import { Outlet } from "react-router-dom"
 
 import ResponsiveAppBar from "./appBar/ResponsiveAppBar"
 
 export default function PageLayout(){
     return(
         <>
-            <Header/>
+            <header>
+                <ResponsiveAppBar/>
+            </header>
             <main>
                 <Outlet/>
             </main>
-            <Footer/>
+            <footer>
+                {/* TODO: Write footer here */}
+            </footer>
         </>
-    )
-}
-
-function Header(){
-    return (
-        <ResponsiveAppBar/>
-    )
-}
-
-function NavBar(){
-    let auth = useAuth()
-    return auth.user ? <LoggedInNavBar/> : <LoggedOutNavBar/>
-}
-
-function LoggedInNavBar(){
-    return (
-        <></>
-    )
-}
-
-function LogOutBtn(){
-    let auth = useAuth()
-    let navigate = useNavigate()
-    
-    const onClick = () => {
-        auth.signOut( () => navigate("/") )
-    }
-
-    return (
-        <button onClick={onClick}>
-            Logg ut
-        </button>
-    )
-}
-
-function LoggedOutNavBar(){
-    return (
-       <></>
-    )
-}
-
-function Separator(){
-    return <>{" "} | {" "}</>
-}
-
-function Footer(){
-    return (
-        <footer>
-        </footer>
     )
 }
