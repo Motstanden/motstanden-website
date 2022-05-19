36b9f2e757d0a0e7ce8dd0c3330cec0b3ef6c56b
Torstein Wilhelmsen Sandvik <torsteinws@gmail.com>
2022-05-19 20:23:18 +0200

    Rename: PageLayout2 -> PageLayout

--- new-client/src/layout/PageLayout.tsx
+++ new-client/src/layout/PageLayout.tsx
@@ -1,90 +1,20 @@
 import React from "react"
-import { Link, Outlet, useNavigate } from "react-router-dom"
-import { useAuth } from "../contextProviders/Authentication"
+import { Outlet } from "react-router-dom"
+
+import ResponsiveAppBar from "./appBar/ResponsiveAppBar"
 
 export default function PageLayout(){
     return(
         <>
-            <Header/>
+            <header>
+                <ResponsiveAppBar/>
+            </header>
             <main>
                 <Outlet/>
             </main>
-            <Footer/>
+            <footer>
+                {/* TODO: Write footer here */}
+            </footer>
         </>
-    )
-}
-
-function Header(){
-    return (
-        <header>
-            Motstanden 
-            <br/>
-            <NavBar/>
-        </header>
-    )
-}
-
-function NavBar(){
-    let auth = useAuth()
-    return auth.user ? <LoggedInNavBar/> : <LoggedOutNavBar/>
-}
-
-function LoggedInNavBar(){
-    return (
-        <nav>
-            <Link to="/hjem">Hjem</Link> 
-            <Separator/>
-            <Link to="/studenttraller">Studenttraller</Link>
-            <Separator/>
-            <Link to="/dokumenter">Dokumenter</Link>
-            <Separator/>
-            <Link to="/sitater">Sitater</Link>
-            <Separator/>
-            <Link to="/notearkiv">Notearkiv</Link>
-            <Separator/>
-            <LogOutBtn/>
-        </nav>
-    )
-}
-
-function LogOutBtn(){
-    let auth = useAuth()
-    let navigate = useNavigate()
-    
-    const onClick = () => {
-        auth.signOut( () => navigate("/") )
-    }
-
-    return (
-        <button onClick={onClick}>
-            Logg ut
-        </button>
-    )
-}
-
-function LoggedOutNavBar(){
-    return (
-        <nav>
-            <Link to="/">Startside</Link>
-            <Separator/>
-            <Link to="/bli-medlem">Bli Medlem</Link>
-            <Separator/>
-            <Link to="/studenttraller">Studenttraller</Link>
-            <Separator/>
-            <Link to="/dokumenter">Dokumenter</Link>
-            <Separator/>
-            <Link to="/logg-inn">Logg inn</Link>
-        </nav>
-    )
-}
-
-function Separator(){
-    return <>{" "} | {" "}</>
-}
-
-function Footer(){
-    return (
-        <footer>
-        </footer>
     )
 }
