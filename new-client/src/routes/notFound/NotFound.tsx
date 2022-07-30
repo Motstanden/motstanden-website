a7c1abc9fdf91130b486e2d72f601ed827774bec
Torstein Wilhelmsen Sandvik <torsteinws@gmail.com>
2022-07-31 01:44:28 +0200

    Create content for 404 page

--- new-client/src/routes/notFound/NotFound.tsx
+++ new-client/src/routes/notFound/NotFound.tsx
@@ -1,9 +1,55 @@
 import React from "react"
+import { PageContainer } from "../../layout/PageContainer"
+import LondonSpyImg from "../../assets/pictures/spyInLondon.png"
 
 export default function NotFound(){
     return (
-        <>
-            <h1>404: Denne siden er i london :(</h1>
-        </>
+        <PageContainer>
+            <div style={{ 
+                fontFamily: "Comic Sans MS, bold, sans-serif",
+                textAlign: "center",
+                marginBottom: "150px"
+            }}>
+                <h1>404</h1>
+                <h1 style={{
+                    color: "#e144f0", 
+                    textShadow: "2px 2px 2px #000000"
+                }}>
+                    Beklager,<br/>siden du leter etter er i London
+                </h1>
+                <img 
+                    src={LondonSpyImg} 
+                    loading="lazy"
+                    alt="Hemmelig spion i London..."
+                    style={{
+                        width: "75%",
+                        boxShadow: "0px 0px 10px 3px #000000",
+                        marginTop: "20px",
+                        marginBottom: "20px"
+                    }}/>
+
+                <h1>Fakta om London på engelsk:</h1>
+                <ul style={{fontSize: 22}}>
+                    <li>
+                        London is the biggest city in Britain and in Europe. 
+                    </li>
+                    <li>
+                        London occupies over 620 square miles
+                    </li>
+                    <li>
+                        London has a population of 7,172,036 (2001) 
+                    </li>
+                    <li>
+                        About 12% of Britain’s overall population live in London (1998)
+                    </li>
+                    <li>
+                        London is in the southeast of England. 
+                    </li>
+                    <li>
+                        The tallest building in London is the Canary Wharf Tower. 
+                    </li>
+                </ul>
+            </div>
+        </PageContainer>
     )
 }
