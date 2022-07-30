7fb27a302550e476eb0a7d7129e8b66351df56da
Torstein Wilhelmsen Sandvik <torsteinws@gmail.com>
2022-07-31 01:19:02 +0200

    Add some content to home route

--- new-client/src/routes/home/Home.tsx
+++ new-client/src/routes/home/Home.tsx
@@ -1,13 +1,15 @@
 import React from "react"
 import { useAuth } from "../../context/Authentication"
+import { PageContainer } from "../../layout/PageContainer"
 
 
 export default function Home(){
     let auth = useAuth()
     return (
-        <>
+        <PageContainer>
             <h1>Hjem</h1>
-            <p>Velkommen {auth.user}</p>
-        </>
+            <p>Velkommen {auth.user}!</p>
+            <p>Her kommer det forhåpentligvis noe kult i fremtiden...</p>
+        </PageContainer>
     )
 }
