c2121ce69c60afaed811cc1bf8afe3eca459ae3c
Torstein Wilhelmsen Sandvik <torsteinws@gmail.com>
2022-08-02 00:41:31 +0200

    Move nodemailer config to separate file

--- server/api/login.js
+++ server/api/login.js
@@ -1,10 +1,6 @@
 import express from "express";
 import passport from "passport";
 
-import nodemailer from "nodemailer"
-import key from "../info-mail-key.json" assert {type: "json" };
-const EmailAddress = "info@motstanden.no" 
-
 const ACCESSTOKEN = "AccessToken"
 
 let router = express.Router()
@@ -35,39 +31,5 @@ router.get("/userMetaData",
     (err, req, res, next) => res.status(204).send("Brukeren er ikke logget inn.")   // Login failure callback
 )
 
-router.post("/auth/magic_login", 
-    async (req, res)=> {
-    await sendMagicLink(req.body.destination)
-    res.send({code: 1234})
-})
-
-async function sendMagicLink(email) {
-    console.log(`Starting to send mail to: ${email}`)
-    const transporter = nodemailer.createTransport({
-        host: "smtp.gmail.com",
-        port: 465,
-        secure: true,
-        auth: {
-            type: "OAuth2",
-            user: EmailAddress,
-            serviceClient: key.client_id,
-            privateKey: key.private_key,
-        },
-    });
-    try {
-        console.log("Verifying...")
-        await transporter.verify();
-        console.log("sending")
-        await transporter.sendMail({
-            from: EmailAddress,
-            to: email,
-            subject: "Hello there",
-            text: "Did you ever hear the Tragedy of Darth Plagueis the Wise?"
-        })
-        console.log("sent")
-    } catch (err){
-        console.log("Error: ", err)
-    }
-}
 
 export default router
---
+++ server/config/mailConfig.js
@@ -0,0 +1,16 @@
+import nodemailer from "nodemailer"
+import key from "../info-mail-key.json" assert {type: "json" };
+
+export const InfoMail = "info@motstanden.no" 
+
+export const transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true,
+    auth: {
+        type: "OAuth2",
+        user: InfoMail,
+        serviceClient: key.client_id,
+        privateKey: key.private_key,
+    },
+});
