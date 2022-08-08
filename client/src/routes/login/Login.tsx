8c7a26f2caf3a00dd06473130ebdf0f80fba7986
Torstein Wilhelmsen Sandvik <torsteinws@gmail.com>
2022-08-08 21:01:46 +0200

    Redirect logged in users away from the login page

--- client/src/routes/login/Login.tsx
+++ client/src/routes/login/Login.tsx
@@ -3,9 +3,19 @@ import React from 'react';
 import { PageContainer } from '../../layout/PageContainer';
 import { useTitle } from '../../hooks/useTitle';
 import { EmailLogin } from './EmailLogin';
+import { useAuth } from 'src/context/Authentication';
+import { Navigate, useLocation } from 'react-router-dom';
 
 export function LoginPage() {
 	useTitle("Logg inn")
+	const auth = useAuth()
+    let location = useLocation()
+
+	if(auth.user){
+		const to = location.pathname === "/logg-inn" ? "/hjem" : location.pathname 
+		return <Navigate  to={to} state={{ from: location }} replace/>
+	}
+
 	return ( 
 		<PageContainer disableGutters >
 			<div style={{textAlign: "center", paddingBottom: "100px"}}>
