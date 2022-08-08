804a11b18baf8de688e6346436e8208c216fc337
Torstein Wilhelmsen Sandvik <torsteinws@gmail.com>
2022-08-08 17:36:22 +0200

    Fallback to first letter of firstname when profile picture fails to load

--- client/src/layout/appBar/UserAvatar.tsx
+++ client/src/layout/appBar/UserAvatar.tsx
@@ -33,7 +33,7 @@ export default function UserAvatar() {
         <>
         <Tooltip title={fullName}>
             <IconButton ref={anchorEl} onClick={() => setIsOpen(!isOpen)}>
-                <Avatar alt={`Profilbilde for ${fullName}`} src={user.profilePicture}>{fullName}</Avatar>
+                <Avatar alt={`Profilbilde for ${fullName}`} src={user.profilePicture}>{user.firstName[0]}</Avatar>
             </IconButton>
         </Tooltip>
         <Menu 
