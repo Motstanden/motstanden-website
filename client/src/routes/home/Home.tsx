736f9b01ab08c59bc3331fb752e20e94722d1a81
Torstein Wilhelmsen Sandvik <torsteinws@gmail.com>
2022-08-10 00:01:12 +0200

    Add quote of the day to home page

--- client/src/routes/home/Home.tsx
+++ client/src/routes/home/Home.tsx
@@ -1,7 +1,13 @@
+import Divider from "@mui/material/Divider"
+import Paper from "@mui/material/Paper"
+import { useQuery } from "@tanstack/react-query"
+import { Quote } from "common/interfaces"
 import React, { useEffect } from "react"
+import { fetchAsync } from "src/utils/fetchAsync"
 import { useAuth } from "../../context/Authentication"
 import { useTitle } from "../../hooks/useTitle"
 import { PageContainer } from "../../layout/PageContainer"
+import { QuoteList } from "../quotes/Quotes"
 
 
 export default function Home(){
@@ -12,7 +18,34 @@ export default function Home(){
         <PageContainer>
             <h1>Hjem</h1>
             <p>Velkommen {user.firstName}!</p>
-            <p>Her kommer det forhåpentligvis noe kult i fremtiden...</p>
+            <br/>
+            <QuoteOfTheDay/>
         </PageContainer>
+    )
+}
+
+function QuoteOfTheDay(){
+    return (
+        <Paper elevation={6} sx={{p: 2, maxWidth: "600px"}}>
+            <h2 style={{margin: 0}} >Dagens sitater</h2>
+            <Divider sx={{mt: 1.5}}/>
+            <QuoteLoader/>
+        </Paper>
+    )
+}
+
+function QuoteLoader(){
+    const {isLoading, isError, data, error} = useQuery<Quote[]>(["FetchQuoteOfTheDay"], () => fetchAsync<Quote[]>("/quotes-of-the-day") )
+    
+    if (isLoading) {
+        return <div style={{minHeight: "100px"}}/>
+    }
+    
+    if (isError) {
+        return <div style={{minHeight: "100px"}}>{`${error}`}</div>
+    }
+
+    return (
+        <QuoteList quotes={data.filter((item, index) => index <= 3)}/>
     )
 }
