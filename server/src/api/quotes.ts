7f3f9299420e4b13a9b6b45662b0a62df8af3476
Torstein Wilhelmsen Sandvik <torsteinws@gmail.com>
2022-08-11 03:32:38 +0200

    Improve randomness in quote of the day

--- server/src/api/quotes.ts
+++ server/src/api/quotes.ts
@@ -28,9 +28,29 @@ router.get("/quotes-of-the-day",
     })
 
 function dailyRandomInt(max: number){
-    const date = new Date()
-    const unlikelyPrimes = 293391909323 // = 71 * 73 * 79 * 83 * 89 * 97 
-    return date.getFullYear() * date.getMonth() * date.getDate() * unlikelyPrimes % max
+    const currentDate = getCurrentDate();
+    const salt = "åæøloxz),m_.!#?¤bxvuesaJM"
+    const salt2 = "Ωvs&<>/*-TC^'$€%I`~"
+    const hashedDate = hashCode(salt2 + currentDate + salt)
+    return Math.abs(hashedDate) % max
+}
+
+function getCurrentDate(): string {
+    return new Date()
+        .toLocaleString("no-no", { timeZone: "cet"})
+        .split(",")[0]
+}
+
+// Generates a fast non-secure simple hash
+// https://stackoverflow.com/questions/6122571/simple-non-secure-hash-function-for-javascript
+function hashCode(str: string): number {
+    let hash = 0;
+    for (let i = 0, len = str.length; i < len; i++) {
+        let chr = str.charCodeAt(i);
+        hash = (hash << 5) - hash + chr;
+        hash |= 0; // Convert to 32bit integer
+    }
+    return hash;
 }
 
 router.post("/insert_quote",    
