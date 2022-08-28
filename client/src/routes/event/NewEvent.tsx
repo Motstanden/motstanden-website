1a149d6564136c256b7c950a4867d87ee8164755
Torstein Wilhelmsen Sandvik <torsteinws@gmail.com>
2022-08-28 19:30:05 +0200

    Use context for event data

--- client/src/routes/event/NewEvent.tsx
+++ client/src/routes/event/NewEvent.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useMemo, useState } from "react"
+import { Reducer, useCallback, useContext, useMemo, useReducer, useState } from "react"
 import { Leaf } from "src/components/TextEditor/Leaf"
 import { Element } from "src/components/TextEditor/Element"
 import { Editable, RenderElementProps, RenderLeafProps, Slate, withReact } from "slate-react"
@@ -16,50 +16,73 @@ import { Toolbar } from "./RichTextEditor"
 import AddIcon from '@mui/icons-material/Add';
 import DeleteIcon from '@mui/icons-material/Delete';
 import { Form } from "src/components/form/Form"
+import React from "react"
 
 export function NewEventPage() {
 
     return (
         <>
             <h1>Nytt arrangement</h1> 
-            <EventForm backUrl="/arrangement" postUrl=""/>
+            <EventForm backUrl="/arrangement" postUrl="" initialValue={emptyEventObj}/>
         </>
     )   
 }
 
-export function EventForm( {backUrl, postUrl}: {backUrl: string, postUrl: string} ) {
+const emptyEventObj: NewEvent = {
+    title: "",
+    startTime: null,
+    endTime: null,
+    extraInfo: [],
+    content: [
+        {
+            type: ElementType.Paragraph,
+            children: [{text: ""}]
+        }
+    ]
+}
+
+const EventStateContext = React.createContext<NewEvent>(null!) 
+const EventDispatchContext = React.createContext<React.Dispatch<Partial<NewEvent>>>(null!)
+function useEvent(): [NewEvent, React.Dispatch<Partial<NewEvent>>] {
+    return [ useContext(EventStateContext), useContext(EventDispatchContext) ]
+}
+
+export function EventForm( {backUrl, postUrl, initialValue}: {backUrl: string, postUrl: string, initialValue: NewEvent} ) {
+
+    const reducer = (state: NewEvent, newVal: Partial<NewEvent>): NewEvent => {
+        return {...state, ...newVal}
+    }
+
     const navigate = useNavigate()
+    const [state, dispatch] = useReducer<Reducer<NewEvent, Partial<NewEvent>>>(reducer, initialValue)
 
     return (
         <Form 
-            value={{}}
+            value={state}       // #TODO: Serialize state
             postUrl={postUrl}
             onAbortClick={ e => navigate(backUrl)}
         >
             <Paper elevation={6} sx={{px: 2, pb: 4, pt: 1}}>
-                <EventEditor/>
+                <EventStateContext.Provider value={state}>
+                   <EventDispatchContext.Provider value={dispatch}>
+                        <EventEditor/>
+                    </EventDispatchContext.Provider> 
+                </EventStateContext.Provider>
             </Paper> 
         </Form>
     )
 }
 
 
-const initialValue: Descendant[] = [
-    {
-        type: ElementType.Paragraph,
-        children: [{text: ""}]
-    }
-]
-
-
 function EventEditor(){
     // const editor = useMemo(() => withHistory(withHistory(withReact(createEditor()))), [])        // Production
+    const [ event, dispatch] = useEvent()
     const [editor] = useState(withHistory(withReact(createEditor())))            // Development
     const renderElement = useCallback( (props: RenderElementProps) => <Element {...props} />, [])
     const renderLeaf = useCallback( (props: RenderLeafProps) => <Leaf {...props}/>, [])
 
     return (
-        <Slate editor={editor}  value={initialValue}>
+        <Slate editor={editor}  value={event.content} onChange={ newVal => dispatch({content: newVal})}>
             <Toolbar/>
             <Divider sx={{mt: 1, mb: 4}}/>
             <EventInfoForm/>
@@ -84,8 +107,8 @@ function EventInfoForm(){
     )
 }
 
-function TitleForm( { sx }: {sx?: SxProps } ) {
-    const [title, setTitle] = useState("")
+function TitleForm( { sx }: { sx?: SxProps } ) {
+    const [ event, dispatch] = useEvent()
     return (
         <TextField
             variant="standard"
@@ -93,8 +116,8 @@ function TitleForm( { sx }: {sx?: SxProps } ) {
             autoComplete="off"
             required
             fullWidth
-            value={title}
-            onChange={e => setTitle(e.target.value)}
+            value={event.title}
+            onChange={e => dispatch({title: e.target.value})}
             InputProps={{ style: {fontSize: "1.5em", fontWeight: "bolder"}}}
             sx={sx}
         />
@@ -103,19 +126,18 @@ function TitleForm( { sx }: {sx?: SxProps } ) {
 
 function TimeForm({ sx }: {sx?: SxProps } ) {
     const today = dayjs()
-    const [startDate, setStartDate] = useState<Dayjs | null>(null)
-    const [endDate, setEndDate] = useState<Dayjs | null>(null)
+    const [ event, dispatch] = useEvent()
     return (
         <Stack direction="row" alignItems="flex-end" sx={sx}>
             <DateTimePicker
                 label="Starter"
                 minDateTime={today}
-                value={startDate}
-                onChange={(newVal: Dayjs | null) => setStartDate(newVal)}
+                value={event.startTime}
+                onChange={(newVal: Dayjs | null) => dispatch({startTime: newVal})}
                 renderInput={ params => ( 
                     <>
                         <strong style={{minWidth: "110px", marginBottom: "5px"}}>Tidspunkt: </strong>
-                        <TextField {...params} required variant="standard" style={{maxWidth: "160px"}}/> 
+                        <TextField {...params} required autoComplete="off" variant="standard" style={{maxWidth: "180px"}}/> 
                     </>)}
                 />
             <div style={{marginInline: "20px", marginBottom: "5px"}}>
@@ -123,13 +145,13 @@ function TimeForm({ sx }: {sx?: SxProps } ) {
             </div>
             <DateTimePicker
                 label="Slutter"
-                disabled={!startDate}
-                minDateTime={startDate ?? today}
-                value={endDate}
-                onChange={(newVal: Dayjs | null) => setEndDate(newVal)}
+                disabled={!event.startTime}
+                minDateTime={event.startTime ?? today}
+                value={event.endTime}
+                onChange={(newVal: Dayjs | null) => dispatch({endTime: newVal})}
                 renderInput={ params => (
                     <>
-                        <TextField {...params} required variant="standard" style={{maxWidth: "160px"}}/>
+                        <TextField {...params} required autoComplete="off" variant="standard" style={{maxWidth: "180px"}}/>
                     </>
                 )}
                 />
@@ -138,21 +160,27 @@ function TimeForm({ sx }: {sx?: SxProps } ) {
 }
 
 function ExtraInfoForm() {
-
-    const [items, setItems] = useState<ExtraInfo[]>([])
+    const [event, dispatch] = useEvent()
 
     const onAddClick = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
-        setItems( prev => [...prev, {key: "", value: ""}])
+        let newItems = [...event.extraInfo, {key: "", value: ""}]
+        dispatch({extraInfo: newItems})
     }
 
     const onDeleteClick = (i: number) => {
-        let newItems = [...items]
+        let newItems = [...event.extraInfo]
         newItems.splice(i, 1)
-        setItems(newItems)
+        dispatch({extraInfo: newItems})
     }
 
+    const onValueChange = (i: number, newVal: ExtraInfo) => {
+        let newItems = [...event.extraInfo]
+        newItems[i] = newVal
+        console.log(i)
+        dispatch({extraInfo: newItems})
+    }
 
-    if(items.length === 0) {
+    if(event.extraInfo.length === 0) {
         return ( 
             <div>
                 <AddInfoButton onClick={onAddClick}/>
@@ -162,7 +190,12 @@ function ExtraInfoForm() {
 
     return (
         <Stack>
-            {items.map( (item, index) => <ExtraInfoItem key={index} value={item} onDeleteClick={() => onDeleteClick(index)} />)}
+            {event.extraInfo.map( (item, index) => <ExtraInfoItem 
+                key={index} 
+                value={item} 
+                onChange={ newVal => onValueChange(index, newVal)}
+                onDeleteClick={() => onDeleteClick(index)} />
+            )}
             <div>
                 <AddInfoButton onClick={onAddClick}/>
             </div>
@@ -170,31 +203,49 @@ function ExtraInfoForm() {
     )
 }
 
-function ExtraInfoItem( {value, onDeleteClick }: {value: ExtraInfo, onDeleteClick?: React.MouseEventHandler<HTMLButtonElement>}) {
+function ExtraInfoItem( {value, onChange, onDeleteClick }: {value: ExtraInfo, onChange: (info: ExtraInfo) => void,  onDeleteClick?: React.MouseEventHandler<HTMLButtonElement>}) {
     return (
         <div>
             <TextField 
+                value={value.key}
                 required 
                 sx={{mb: 4}}
                 style={{maxWidth: "100px", marginRight: "10px"}}
                 InputProps={{ style: {fontWeight: "bold"}}}
                 variant="standard"
                 placeholder="Tittel*"
+                onChange={ e => onChange({...value, key: e.target.value})}
             />
-            <TextField required variant="standard" placeholder="info*" /> 
+            <TextField 
+                value={value.value}
+                required 
+                variant="standard" 
+                placeholder="info*" 
+                onChange={ e => onChange({...value, value: e.target.value})}
+            /> 
             <IconButton style={{marginLeft: "20px"}} onClick={onDeleteClick}>
                 <DeleteIcon/>
             </IconButton>
         </div>
     )
 }
-interface ExtraInfo{ 
-    key: string,
-    value: string,
-}
+
 
 function AddInfoButton({onClick}: {onClick?: React.MouseEventHandler<HTMLButtonElement>}) {
     return (
         <Button variant="outlined" endIcon={<AddIcon/>} size="small" onClick={onClick} >Info</Button>
-    )
+        )
+    }
+    
+interface NewEvent {
+    title: string
+    startTime: Dayjs | null,
+    endTime: Dayjs | null,
+    extraInfo: ExtraInfo[]
+    content: Descendant[]
+}
+
+interface ExtraInfo{ 
+    key: string,
+    value: string,
 }
