73e17b4c31cbde25f600b136a723332d3571b692
Torstein Wilhelmsen Sandvik <torsteinws@gmail.com>
2022-09-15 17:58:42 +0200

    Setup routing to license pages

---
+++ client/src/routes/license/LicensePage.tsx
@@ -0,0 +1,17 @@
+import { PageContainer } from "src/layout/PageContainer";
+
+export function LicensePage() {
+    return (
+        <PageContainer>
+            <h1>Lisens</h1>
+        </PageContainer>
+    )
+}
+
+export function LicenseTextPage() {
+    return (
+        <PageContainer>
+            <h1>Måkesodd</h1>
+        </PageContainer>
+    )
+}
