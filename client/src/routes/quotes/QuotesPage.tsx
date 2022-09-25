55625197868c100554dff13348e5a36c67254d74
Torstein Wilhelmsen Sandvik <torsteinws@gmail.com>
2022-09-25 03:03:30 +0200

    Add visual feedback when deleting quote

--- client/src/routes/quotes/QuotesPage.tsx
+++ client/src/routes/quotes/QuotesPage.tsx
@@ -35,6 +35,7 @@ export function QuoteList( {quotes}: {quotes: QuoteData[] } ){
 function QuoteItem( {quoteData}: {quoteData: QuoteData}){
 
     const [isHighlighted, setIsHighlighted] = useState(false)
+    const [isDisabled, setIsDisabled] = useState(false)
 
     const onMouseEnter = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
         setIsHighlighted(true)
@@ -52,6 +53,14 @@ function QuoteItem( {quoteData}: {quoteData: QuoteData}){
         setIsHighlighted(false)
     }
 
+    const onDeleting = () => {
+        setIsDisabled(true)
+    }
+
+    const onDeleteFailure = () => {
+        setIsDisabled(false)
+    }
+
     const user = useAuth().user!
     const hasEditPrivilege = user.userId === quoteData.userId || hasGroupAccess(user, UserGroup.Administrator) 
 
@@ -60,10 +69,13 @@ function QuoteItem( {quoteData}: {quoteData: QuoteData}){
             <Stack 
                 direction="row" 
                 justifyContent="space-between" 
-                bgcolor={isHighlighted ? "action.hover" : "transparent"}
+                bgcolor={isHighlighted || isDisabled ? "action.hover" : "transparent"}
                 pl={1}
                 ml={-1}
-                style={{borderRadius: "5px"}}
+                style={{
+                    borderRadius: "5px", 
+                    opacity: isDisabled ? 0.4 : 1
+                }}
                 >
                 <div style={{marginBlock: "10px"}}>
                     <NewlineText text={quoteData.quote}/>
@@ -81,6 +93,9 @@ function QuoteItem( {quoteData}: {quoteData: QuoteData}){
                     <div>
                         <ItemMenu 
                             quoteData={quoteData} 
+                            disabled={isDisabled}
+                            onDeleting={onDeleting}
+                            onDeleteFailure={onDeleteFailure}
                             onMouseEnter={onMouseEnter} 
                             onMouseLeave={onMouseLeave} 
                             onMenuOpen={onMenuOpen} 
@@ -97,13 +112,19 @@ function ItemMenu( {
     onMouseEnter,
     onMouseLeave,
     onMenuOpen,
-    onMenuClose
+    onMenuClose,
+    onDeleting,
+    onDeleteFailure,
+    disabled
 }: {
     quoteData: QuoteData,
     onMouseEnter?: React.MouseEventHandler<HTMLButtonElement> | undefined,
     onMouseLeave?: React.MouseEventHandler<HTMLDivElement> | undefined,
     onMenuOpen?: VoidFunction,
-    onMenuClose?: VoidFunction
+    onMenuClose?: VoidFunction,
+    onDeleting?: VoidFunction,
+    onDeleteFailure?: VoidFunction,
+    disabled?: boolean
 }){
 
     const onEditClick = (e: React.MouseEvent<HTMLLIElement, MouseEvent>) => {
@@ -111,6 +132,7 @@ function ItemMenu( {
     }
 
     const onDeleteClick = async (e: React.MouseEvent<HTMLLIElement, MouseEvent>) => {
+        onDeleting && onDeleting()
         const response = await postJson(
             "/api/quotes/delete", 
             {quoteId: quoteData.id},
@@ -119,11 +141,15 @@ function ItemMenu( {
                 confirmText: `Vil du permanent slette sitatet?`
             }
         )
+        if (!response?.ok){
+            onDeleteFailure && onDeleteFailure()
+        }
     }
 
     return (
         <div onMouseLeave={onMouseLeave}>
             <EditOrDeleteMenu 
+                disabled={disabled}
                 onEditClick={onEditClick} 
                 onDeleteClick={onDeleteClick} 
                 onMouseEnter={onMouseEnter} 
