7ed7536d20cef0f2a6a72278c4c26fcdec45ee80
Torstein Wilhelmsen Sandvik <torsteinws@gmail.com>
2022-09-27 21:01:10 +0200

    Move daily random into to separate file

--- server/src/api/quotes.ts
+++ server/src/api/quotes.ts
@@ -6,6 +6,7 @@ import { AccessTokenData } from "../ts/interfaces/AccessTokenData.js";
 import { hasGroupAccess } from "../utils/accessTokenUtils";
 import { UserGroup } from "common/enums";
 import { requiresGroupOrAuthor } from "../middleware/requiresGroupOrAuthor.js";
+import dailyRandomInt from "../utils/dailyRandomInt.js";
 
 let router = express.Router()
 
@@ -27,32 +28,6 @@ router.get("/quotes-of-the-day",
             quotes[(i + 2) % mod]
         ])
     })
-
-function dailyRandomInt(max: number){
-    const currentDate = getCurrentDate();
-    const salt = "åæøloxz),m_.!#?¤bxvuesaJM"
-    const salt2 = "Ωvs&<>/*-TC^'$€%I`~"
-    const hashedDate = hashCode(salt2 + currentDate + salt)
-    return Math.abs(hashedDate) % max
-}
-
-function getCurrentDate(): string {
-    return new Date()
-        .toLocaleString("no-no", { timeZone: "cet"})
-        .split(",")[0]
-}
-
-// Generates a fast non-secure simple hash
-// https://stackoverflow.com/questions/6122571/simple-non-secure-hash-function-for-javascript
-function hashCode(str: string): number {
-    let hash = 0;
-    for (let i = 0, len = str.length; i < len; i++) {
-        let chr = str.charCodeAt(i);
-        hash = (hash << 5) - hash + chr;
-        hash |= 0; // Convert to 32bit integer
-    }
-    return hash;
-}
 
 router.post("/insert_quote",    
     AuthenticateUser(),
