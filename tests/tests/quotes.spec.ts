bc4040f80e75297d0acf33b32560bf5c24dbf440
Torstein Wilhelmsen Sandvik <torsteinws@gmail.com>
2022-10-23 17:20:52 +0200

    Refactor quotes to use test steps

--- tests/tests/quotes.spec.ts
+++ tests/tests/quotes.spec.ts
@@ -4,59 +4,68 @@ import { NewQuote } from "common/interfaces";
 import { randomUUID } from "crypto";
 import { logIn } from './auth.spec';
 
-test('Quotes can be created, updated and deleted', async ({ page }) => {
 
-    await logIn(page, UserGroup.Contributor)
-
-    await page.goto('/sitater/ny');
-
-    // Test if quote can be inserted
+test("Quotes can be created, updated and deleted", async ({page}) => {
+    
+    // Mockup data for the tests
     const newQuote: NewQuote = { 
         utterer: `utterer id: ${randomUUID()}`, 
         quote: `quote id: ${randomUUID()}` 
     }
-    
-    await page.getByLabel('Sitat *').click();
-    await page.getByLabel('Sitat *').fill(newQuote.quote);
-
-    await page.getByLabel('Sitatytrer *').click();
-    await page.getByLabel('Sitatytrer *').fill(newQuote.utterer);
-
-    await page.getByRole('button', { name: 'Lagre' }).click();
-    await expect(page).toHaveURL('/sitater');
-
-    await expect(page.getByText(newQuote.quote)).toBeVisible();
-    await expect(page.getByText(newQuote.utterer)).toBeVisible();
-
-
-    // Test if quote can be edited
     const editedQuote: NewQuote = { 
         utterer: `utterer id: ${randomUUID()}`, 
         quote: `quote id: ${randomUUID()}` 
     }
 
-    await page.locator(`li:has-text("${newQuote.quote}")`).getByRole('button').click();
+    await test.step("Log in", async () => {
+        await logIn(page, UserGroup.Contributor)
+    })
 
-    await page.getByRole('menuitem', { name: 'Rediger' }).click();
-  
-    await page.getByLabel('Sitat *').click();
-    await page.getByLabel('Sitat *').fill(editedQuote.quote);
-  
-    await page.getByLabel('Sitatytrer *').click();
-    await page.getByLabel('Sitatytrer *').fill(editedQuote.utterer);
-  
-    await page.getByRole('button', { name: 'Lagre' }).click();
-  
-    await expect(page.getByText(editedQuote.quote)).toBeVisible();
-    await expect(page.getByText(editedQuote.utterer)).toBeVisible();
+    await test.step("New quote", async () => {
+        await page.goto('/sitater/ny');
+        await page.getByLabel('Sitat *').click();
+        await page.getByLabel('Sitat *').fill(newQuote.quote);
+    
+        await page.getByLabel('Sitatytrer *').click();
+        await page.getByLabel('Sitatytrer *').fill(newQuote.utterer);
+    
+        await page.getByRole('button', { name: 'Lagre' }).click();
+        await expect(page).toHaveURL('/sitater');
+    
+        await expect(page.getByText(newQuote.quote)).toBeVisible();
+        await expect(page.getByText(newQuote.utterer)).toBeVisible();
+    })
 
-    await page.locator(`li:has-text("${editedQuote.quote}")`).getByRole('button').click();
+    await test.step("Update quote", async () => {
+        await page.goto('/sitater');
+        await page.locator(`li:has-text("${newQuote.quote}")`).getByRole('button').click();
 
-    // Delete quote
-    page.on('dialog', dialog => dialog.accept());
+        await page.getByRole('menuitem', { name: 'Rediger' }).click();
+      
+        await page.getByLabel('Sitat *').click();
+        await page.getByLabel('Sitat *').fill(editedQuote.quote);
+      
+        await page.getByLabel('Sitatytrer *').click();
+        await page.getByLabel('Sitatytrer *').fill(editedQuote.utterer);
+      
+        await page.getByRole('button', { name: 'Lagre' }).click();
+      
+        await expect(page.getByText(editedQuote.quote)).toBeVisible();
+        await expect(page.getByText(editedQuote.utterer)).toBeVisible();
+        await expect(page.getByText(newQuote.quote)).not.toBeVisible();
+        await expect(page.getByText(newQuote.utterer)).not.toBeVisible();
+    })
 
-    await page.getByRole('menuitem', { name: 'Slett' }).click();
-  
-    await expect(page.getByText(editedQuote.quote)).not.toBeVisible();
-    await expect(page.getByText(editedQuote.utterer)).not.toBeVisible();
-});
+    await test.step("Delete quote", async () => {
+        await page.goto('/sitater');
+
+        page.on('dialog', dialog => dialog.accept());
+
+        await page.locator(`li:has-text("${editedQuote.quote}")`).getByRole('button').click();
+        
+        await page.getByRole('menuitem', { name: 'Slett' }).click();
+      
+        await expect(page.getByText(editedQuote.quote)).not.toBeVisible();
+        await expect(page.getByText(editedQuote.utterer)).not.toBeVisible();
+    })
+})
