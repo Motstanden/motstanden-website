ff12d336187c3d07dbeed12b864e0d4aff915bec
Torstein Wilhelmsen Sandvik <torsteinws@gmail.com>
2022-10-26 20:07:04 +0200

    Test that login tokens are created

--- tests/tests/auth.spec.ts
+++ tests/tests/auth.spec.ts
@@ -25,6 +25,50 @@ export function getUserEmail(group: UserGroup): string {
     }
 }
 
+test.describe("Login tokens are created and persisted", () => {
+
+    test("Should create AccessToken and RefreshTokens on login", async ({ page }) => {
+        await logIn(page, UserGroup.Contributor)
+        const cookies = await page.context().cookies()
+        
+        expect(cookies.length).toBe(2)
+        
+        const accessToken = cookies.find(c => c.name === "AccessToken")
+        expect(accessToken).toBeDefined()
+
+        const refreshToken = cookies.find(c => c.name === "RefreshToken")
+        expect(refreshToken).toBeDefined()
+    })
+
+    test("AccessToken expires within 15 minutes", async ({page}) => {
+
+        await logIn(page, UserGroup.Contributor)
+
+        const cookies = await page.context().cookies()
+        const accessToken = cookies.find(c => c.name === "AccessToken")
+
+        // Test that AccessToken expires within 15 minutes
+        expect(accessToken.expires * 1000).toBeLessThanOrEqual(Date.now() + 1000*60*15)
+    })
+
+    test("Should renew AccessToken if browser only has RefreshToken", async ({page}) => {
+        await logIn(page, UserGroup.Contributor)
+
+        const initialCookies = await page.context().cookies()
+        const expiredCookies = initialCookies.filter( c => c.name !== "AccessToken")
+        await page.context().clearCookies()
+        await page.context().addCookies(expiredCookies)
+
+        await page.goto("/sitater")
+        await expect(page).toHaveURL("/sitater")
+
+        const newCookies = await page.context().cookies()
+        const accessToken = newCookies.find(c => c.name === "AccessToken")
+        expect(accessToken).toBeDefined()
+    })
+
+})
+
 test.describe("User can log out", () => {
     test("Log out in current browser", async ({ page }) => {
         await emailLogIn(page, "web@motstanden.no")
