3a3567dddc70425230b5ecd6cdeec8dfb3869f56
Torstein Wilhelmsen Sandvik <torsteinws@gmail.com>
2022-12-06 22:35:43 +0100

    Add testing mode

--- client/src/vite-env.d.ts
+++ client/src/vite-env.d.ts
@@ -1,1 +1,5 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+    readonly VITE_ENABLE_DEV_LOGIN: string
+}
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,30 +3,33 @@ import path from 'path'
 import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-    plugins: [react()],
-    server: {
-        port: 3000,
-        host: true,             // I have no idea why, but this option is necessary for playwright (v1.28.1) to run tests with firefox and webkit
-        strictPort: true,
-        open: true,
-        proxy: {
-            "/api": {
-                target: "http://localhost:5000",
-                changeOrigin: true,
-            },
-            "/files": {
-                target: "http://localhost:5000",
-                changeOrigin: true,
+export default defineConfig( ({mode}) => {
+    return {
+        plugins: [react()],
+        server: {
+            port: mode === "testing" ? 3001 : 3000,
+            strictPort: true,
+            host: true,             // I have no idea why, but this option is necessary for playwright (v1.28.1) to run tests with firefox and webkit
+            open: true,
+            https: false,
+            proxy: {
+                "/api": {
+                    target: "http://localhost:5000",
+                    changeOrigin: true,
+                },
+                "/files": {
+                    target: "http://localhost:5000",
+                    changeOrigin: true,
+                }
             }
+        }, 
+        build: {
+            outDir: "build"
+        },
+        resolve: {
+            alias: [
+                { find: "src", replacement: path.resolve(__dirname, "src") } 
+            ]
         }
-    }, 
-    build: {
-        outDir: "build"
-    },
-    resolve: {
-        alias: [
-            { find: "src", replacement: path.resolve(__dirname, "src") } 
-        ]
     }
 })
