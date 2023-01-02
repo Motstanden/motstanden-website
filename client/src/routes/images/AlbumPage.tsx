66b5e4737a3cf0c411a809f8b7aee6dc5ae7fafa
Torstein Wilhelmsen Sandvik <torsteinws@gmail.com>
2023-01-02 22:33:39 +0100

    [WIP] create proof of concept for image lighbox

--- client/src/routes/images/AlbumPage.tsx
+++ client/src/routes/images/AlbumPage.tsx
@@ -1,9 +1,20 @@
-import { ImageList, ImageListItem, Theme, useMediaQuery } from "@mui/material";
-import { ImageAlbum } from "common/interfaces";
+import { ImageList, ImageListItem, Modal, Theme, useMediaQuery } from "@mui/material";
+import { Image, ImageAlbum } from "common/interfaces";
+import { useState } from "react";
 import { useOutletContext } from "react-router-dom";
 
 export default function AlbumPage() {
     const album: ImageAlbum = useOutletContext<ImageAlbum>()
+
+    return (
+        <>
+            <h1>{album.title}</h1>
+            <AlbumViewer album={album}/>
+        </>
+    )
+}
+
+function AlbumViewer( { album }: { album: ImageAlbum }  ) {
 
     const isSmallScreen = useMediaQuery((theme: Theme) => theme.breakpoints.down('sm'));
     const isLargeScreen = useMediaQuery((theme: Theme) => theme.breakpoints.up("lg"))
@@ -11,16 +22,57 @@ export default function AlbumPage() {
     let listProps = {cols: 3, gap: 20}
     if( isSmallScreen ) listProps = { cols: 2, gap: 10 };
     if( isLargeScreen ) listProps = { cols: 4, gap: 30}
+
     return (
-        <>
-            <h1>{album.title}</h1>
-            <ImageList {...listProps} >
-                {album.images.map( image => (
-                    <ImageListItem key={image.url}>
-                        <img src={`/${image.url}`} loading="lazy"/>
-                    </ImageListItem>
-                ))}
-            </ImageList>
-        </>
+        <ImageList {...listProps} >
+            {album.images.map( image => (
+                <ImageViewer image={image} key={image.url} />
+            ))}
+        </ImageList>
+    )
+}
+
+
+
+function ImageViewer( {image}: {image: Image}) {
+    const [open, setOpen] = useState(false);
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
+
+    return (
+        <ImageListItem>
+            <img 
+                src={`/${image.url}`} 
+                loading="lazy"
+                onClick={handleOpen}
+                />
+            <Modal 
+                open={open} 
+                onClose={handleClose} 
+                slotProps={{
+                    backdrop: { 
+                        style: {
+                            backgroundColor: "rgba(0, 0, 0, 0.9)"
+                        }
+                    }
+                }}  >
+                <ImageModalPage image={image} />
+            </Modal>
+        </ImageListItem>
+    )
+}
+
+function ImageModalPage( {image}: {image: Image}) {
+    return (
+        <img 
+            src={`/${image.url}`} 
+            style={{
+                maxWidth: "100vw", 
+                maxHeight: "100vh",
+                position: "absolute",
+                top: "50%",
+                left: "50%",
+                transform: 'translate(-50%, -50%)',
+            }}/>
     )
 }
