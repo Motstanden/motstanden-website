c3b043ad427a994f30eb14448c7329b6d47725ad
Torstein Wilhelmsen Sandvik <torsteinws@gmail.com>
2023-01-02 20:05:27 +0100

    Use mui image list for displaying images

--- client/src/routes/images/AlbumPage.tsx
+++ client/src/routes/images/AlbumPage.tsx
@@ -1,18 +1,26 @@
-import { ImageAlbum } from "common/interfaces"
-import { useOutletContext } from "react-router-dom"
+import { ImageList, ImageListItem, Theme, useMediaQuery } from "@mui/material";
+import { ImageAlbum } from "common/interfaces";
+import { useOutletContext } from "react-router-dom";
 
 export default function AlbumPage() {
     const album: ImageAlbum = useOutletContext<ImageAlbum>()
+
+    const isSmallScreen = useMediaQuery((theme: Theme) => theme.breakpoints.down('sm'));
+    const isLargeScreen = useMediaQuery((theme: Theme) => theme.breakpoints.up("lg"))
+
+    let listProps = {cols: 3, gap: 20}
+    if( isSmallScreen ) listProps = { cols: 2, gap: 10 };
+    if( isLargeScreen ) listProps = { cols: 4, gap: 30}
     return (
         <>
             <h1>{album.title}</h1>
-            <ul>
+            <ImageList {...listProps} >
                 {album.images.map( image => (
-                    <li key={image.url}>
-                        <img src={`/${image.url}`} height="150px"/>
-                    </li>
+                    <ImageListItem key={image.url}>
+                        <img src={`/${image.url}`} loading="lazy"/>
+                    </ImageListItem>
                 ))}
-            </ul>
+            </ImageList>
         </>
     )
 }
