09a0f0a068f317215be00f18bdaa5bf2d8e0e7c0
Torsteinws <47348352+Torsteinws@users.noreply.github.com>
2023-04-15 03:19:40 +0200

    Update Footer.tsx

--- client/src/layout/Footer.tsx
+++ client/src/layout/Footer.tsx
@@ -69,7 +69,7 @@ function MotstandenInfo() {
                 <Typography>
                     Studentorchesteret <wbr />
                     <NoBr>den Ohmske</NoBr> <wbr />
-                    Motstanden
+                    Motstanden - Jeg hater at jeg må skrive dette i et tre 😞
                 </Typography>
             </Grid>
             <Grid item xs={12} sm={4} md={3}>
