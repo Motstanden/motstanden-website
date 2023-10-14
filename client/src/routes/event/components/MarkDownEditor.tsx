8b2e07f2c9daa354d0a40b04a117010a72acf94d
Torstein Wilhelmsen Sandvik <torsteinws@gmail.com>
2023-10-14 16:36:17 +0200

    Set up structure for md editor

--- client/src/routes/event/components/MarkDownEditor.tsx
+++ client/src/routes/event/components/MarkDownEditor.tsx
@@ -1,18 +1,42 @@
 import { TextField } from "@mui/material"
+import { useState } from "react"
 
-export function MarkDownEditor( {
-    value,
-    onChange,
-    label,
-    required,
-    minRows,
-}: {
+interface MarkDownEditorProps {
     value?: string,
     onChange?: ( value: string ) => void
     label?: string
     required?: boolean,
     minRows?: number
-} ) {
+
+}
+
+export function MarkDownEditor( props: MarkDownEditorProps) {
+
+    const [isPreview, setIsPreview] = useState(false)
+
+    return (
+        <div>
+            <ContentPicker 
+                {...props} 
+                isPreview={isPreview}
+            />
+        </div>
+    )
+}
+
+interface ContentPickerProps extends MarkDownEditorProps {
+    isPreview: boolean
+}
+
+function ContentPicker(props: ContentPickerProps){
+    const {isPreview, ...mdProps} = props
+    return isPreview 
+        ? <MarkDownReader/> 
+        : <MarkDownWriter {...mdProps}/>
+}
+
+function MarkDownWriter( props: MarkDownEditorProps ) {
+    const { value, onChange, label, required, minRows } = props
 
     const onValueChange = ( e: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement> ) => {
         if(onChange !== undefined){
@@ -33,6 +57,14 @@ export function MarkDownEditor( {
                 onChange={onValueChange}
                 minRows={minRows}
             />
+        </div>
+    )
+}
+
+function MarkDownReader(){
+    return (
+        <div>
+            Kommer snart...
         </div>
     )
 }
