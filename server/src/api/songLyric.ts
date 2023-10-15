ed0cb5553f196e75e29807fc09b8c8bb6f4bfeca
Torstein Wilhelmsen Sandvik <torsteinws@gmail.com>
2023-10-15 23:52:25 +0200

    Serve lyric from db

--- server/src/api/songLyric.ts
+++ server/src/api/songLyric.ts
@@ -4,55 +4,24 @@ import fs from "fs";
 import { fileURLToPath } from "url";
 import { dbReadOnlyConfig, motstandenDB } from "../config/databaseConfig.js";
 import { songLyricService } from "../services/songLyric.js";
+import { strToNumber } from "common/utils";
 
 const router = express.Router()
 
 router.get("/song-lyric/simple-list", (req, res) => { 
-    const lyrics = songLyricService.getList()
+    const lyrics = songLyricService.getSimpleList()
     res.send(lyrics)
 })
 
-router.get("/song-lyric/:title", (req, res) => {
-    res.send({ id: 0, title: req.params.title, content: "TODO..." })
-})
-
-router.get("/song_lyric", (req, res) => {
-    const db = new Database(motstandenDB, dbReadOnlyConfig)
-    const stmt = db.prepare("SELECT \
-                                title, \
-                                filename as url, \
-                                song_melody AS melody, \
-                                song_text_origin AS textOrigin, \
-                                song_description AS description \
-                            FROM song_lyric ORDER BY title")
-    const lyrics = stmt.all()
-    res.send(lyrics)
-    db.close()
-})
-
-router.get("/song_lyric_data", (req, res) => {
-
-    // Get the name of the html file
-    const db = new Database(motstandenDB, dbReadOnlyConfig)
-    const stmt = db.prepare("SELECT filename AS file FROM song_lyric WHERE title = ?")
-    const title = req.query.title;
-    const rows = stmt.get([title])
-    if (!rows) {
-        res.status(400)
-        res.end()
+router.get("/song-lyric/:id", (req, res) => {
+    const id = strToNumber(req.params.id) as number
+    try {
+        const lyric = songLyricService.get(id)
+        res.send(lyric)
+    } catch (err) {
+        console.log(err)
+        res.status(500).end()
     }
-    const filename = fileURLToPath(new URL(`../../${rows.file}`, import.meta.url))
-    db.close()
-
-    // Send html file as string
-    fs.readFile(filename, (err, data) => {
-        if (err) {
-            res.status(400)
-        }
-        else {
-            res.json({ lyricHtml: data.toString() })
-        }
-    });
 })
 
 export default router;
--- server/src/services/songLyric.ts
+++ server/src/services/songLyric.ts
@@ -1,5 +1,5 @@
 import Database from "better-sqlite3"
-import { StrippedSongLyric } from "common/interfaces"
+import { SongLyric, StrippedSongLyric } from "common/interfaces"
 import { dbReadOnlyConfig, motstandenDB } from "../config/databaseConfig.js"
 
 function getSimpleList(): StrippedSongLyric[] {
@@ -19,6 +19,29 @@ function getSimpleList(): StrippedSongLyric[] {
     return lyrics
 }
 
+function get(id: number): SongLyric {
+    const db = new Database(motstandenDB, dbReadOnlyConfig)
+    const stmt = db.prepare(`
+        SELECT
+            song_lyric_id as id,
+            title,
+            content
+        FROM 
+            song_lyric 
+        WHERE 
+            song_lyric_id = ?
+    `) 
+
+    const lyric: SongLyric | undefined = stmt.get(id)
+    db.close()
+
+    if(!lyric)
+        throw "Song lyric not found"
+
+    return lyric
+}
+
 export const songLyricService = {
-    getList: getSimpleList
+    getSimpleList: getSimpleList,
+    get: get,
 }
