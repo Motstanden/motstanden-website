0491d4aa58a4830c68e6301a87da4ffeb9070d4a
Torstein Wilhelmsen Sandvik <torsteinws@gmail.com>
2023-10-17 14:27:54 +0200

    Insert front page text into simple text table

---
+++ database/data/db/InsertSimpleText.sql
@@ -0,0 +1,29 @@
+INSERT INTO 
+    simple_text(key, updated_by, text)
+VALUES
+    (
+        'front-page',
+        14,
+'# Om oss
+
+Studentorchesteret den Ohmske Motstanden er et studentorchester som tilhører Linjeforeningen Elektra ved NTNU i Trondheim. Hovedformålet til Motstanden er å tilby et lavterskel musikalsk- sosialtilbud for medlemmer av Elektra.
+
+# Bli medlem?
+
+Alle som er medlem i Elektra kan bli medlem i Motstanden. Det stilles overhodet ingen krav til ferdigheter for å bli med. Kan du spille et instrument så er det kjempeflott, men musikalsk erfaring er absolutt ikke et krav! Det viktigste i Motstanden er å ha det gøy og å være sosial!
+
+# Øvelse
+
+**Motstanden har øvelse på Elektrakontoret hver torsdag fra 19:00 til 21:00.**
+
+Etter øvelsen pleier vi å dra på Samfundet eller finne på noe sosialt på kontoret. Øvelsene er åpne for alle, så det er bare å møte opp med godt humør, **og gjerne et par pils...**
+
+# Kontakt oss
+
+Er det noe du lurer på? Kontakt oss gjerne på:
+
+- [styret@motstanden.no](mailto:styret@motstanden.no)
+- [Facebook](https://www.facebook.com/Motstanden)
+- [Instagram](https://instagram.com/denohmskemotstanden)
+- [TikTok](https://vm.tiktok.com/ZMNVv1Tk3/)'
+    );
--- database/data/db/motstanden-db-data.sh
+++ database/data/db/motstanden-db-data.sh
@@ -21,3 +21,6 @@ sqlite3 ../../motstanden_dev.db < InsertRumours.sql;
 
 echo "    Inserting polls";
 sqlite3 ../../motstanden_dev.db < InsertPolls.sql;
+
+echo "    Inserting simple text";
+sqlite3 ../../motstanden_dev.db < InsertSimpleText.sql;
