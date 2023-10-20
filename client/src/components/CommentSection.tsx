dbeda8fd1aab24159ae8d8ea981b8a9da93f04ed
Torstein Wilhelmsen Sandvik <torsteinws@gmail.com>
2023-10-20 16:58:58 +0200

    WIP: Create some basic styling for comments

--- client/src/components/CommentSection.tsx
+++ client/src/components/CommentSection.tsx
@@ -1,11 +1,38 @@
+import { Avatar, Stack, useTheme } from "@mui/material"
 import { useQuery } from "@tanstack/react-query"
 import { CommentEntityType } from "common/enums"
+import { Comment } from "common/interfaces"
+import dayjs from "dayjs"
 import { fetchAsync } from "src/utils/fetchAsync"
 
 
-export { 
-    CommentSectionFetcher as CommentSection
-} 
+export {
+    CommentSectionContainer as CommentSection
+}
+
+function CommentSectionContainer({
+    entityType,
+    entityId,
+    style,
+}: {
+    entityType: CommentEntityType,
+    entityId: number
+    style?: React.CSSProperties
+}){
+    return (
+        <section
+            style={{
+                maxWidth: "600px",
+                ...style
+            }}
+        >
+            <CommentSectionFetcher
+                entityId={entityId}
+                entityType={entityType}
+            />
+        </section>
+    )
+}
 
 function CommentSectionFetcher({
     entityType,
@@ -40,8 +67,71 @@ function CommentSectionSkeleton() {
 }
 
 function CommentSection( {comments}: {comments: Comment[]}) {
-    console.log(comments)
     return (
-        <>Her kommer de kommentarfelt</>
+        <>
+            {comments.map(comment => (
+                <CommentItem 
+                    key={comment.id}
+                    comment={comment}
+                    style={{
+                        marginBottom: "15px",
+                    }}
+                />
+            ))}
+        </>
+    )
+}
+
+function CommentItem( {
+    comment,
+    style
+}: {
+    comment: Comment,
+    style?: React.CSSProperties
+}) {
+    const theme = useTheme()
+    return (
+        <Stack 
+            direction="row"
+            spacing={2}
+            style={style}
+        >
+            <Avatar 
+                style={{
+                    marginTop: "5px"
+                }}
+            >
+                {comment.createdBy}
+            </Avatar>
+            <div
+                style={{
+                    width: "100%"
+                }}
+            >
+                <div
+                    style={{
+                        backgroundColor: theme.palette.divider,
+                        padding: "12px",
+                        borderRadius: "10px",
+                    }}
+                >
+                    <div>
+                        <b>{comment.createdBy}</b>
+                    </div>
+                    <div>
+                        {comment.comment}
+                    </div>
+                </div>
+                <div
+                    style={{
+                        marginLeft: "5px",
+                        fontSize: "small",
+                        opacity: "0.6"
+                    }}
+                >
+                    {dayjs(comment.createdAt).fromNow()}
+                </div>
+            </div>
+        </Stack>
     )
 }
