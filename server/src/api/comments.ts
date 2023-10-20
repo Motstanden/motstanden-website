577e12f4af7d71d360d2d429dfae1b5d3d97b988
Torstein Wilhelmsen Sandvik <torsteinws@gmail.com>
2023-10-20 15:17:56 +0200

    Hoist responsibility for getting and parsing entityId

--- server/src/api/comments.ts
+++ server/src/api/comments.ts
@@ -1,53 +1,79 @@
 import express, {Request, Response}from "express";
 import { AuthenticateUser } from "../middleware/jwtAuthenticate.js";
 import { CommentEntityType } from "common/enums";
+import { validateNumber } from "../middleware/validateNumber.js";
+import { strToNumber } from "common/utils";
 
 const router = express.Router()
 
 router.get("/event/:entityId/comments",
     AuthenticateUser(),
+    validateNumber({
+        getValue: (req: Request) => req.params.entityId,
+    }),
     getCommentsHandler({
-        entity: CommentEntityType.Event
+        entityType: CommentEntityType.Event,
+        getEntityId: (req: Request) => strToNumber(req.params.entityId) as number
     })
 )
 
 router.get("/poll/:entityId/comments",
     AuthenticateUser(),
+    validateNumber({
+        getValue: (req: Request) => req.params.entityId,
+    }),
     getCommentsHandler({
-        entity: CommentEntityType.Poll
+        entityType: CommentEntityType.Poll,
+        getEntityId: (req: Request) => strToNumber(req.params.entityId) as number
     })
 )
 
-function getCommentsHandler( {entity}: {entity: CommentEntityType}) {
+function getCommentsHandler( {
+    entityType,
+    getEntityId
+}: {
+    entityType: CommentEntityType,
+    getEntityId: (req: Request) => number
+}) {
     return (req: Request, res: Response) => {
         // TODO
-
-        console.log("Getting comments for entity: ", entity, " with id: ", req.params.entityId)
-
-        throw "Not implemented yet"
+        
     }
 }
 
-
 router.post("/event/:entityId/comments",
     AuthenticateUser(),
+    validateNumber({
+        getValue: (req: Request) => req.params.entityId,
+    }),
     postCommentHandler({
-        entity: CommentEntityType.Event
+        entityType: CommentEntityType.Event,
+        getEntityId: (req: Request) => strToNumber(req.params.entityId) as number
     })
 )
 
 router.post("/poll/:entityId/comments", 
     AuthenticateUser(),
+    validateNumber({
+        getValue: (req: Request) => req.params.entityId,
+    }),
     postCommentHandler({
-        entity: CommentEntityType.Poll
+        entityType: CommentEntityType.Poll,
+        getEntityId: (req: Request) => strToNumber(req.params.entityId) as number
     })
 )
 
-function postCommentHandler( {entity}: {entity: CommentEntityType}) {
+function postCommentHandler( {
+    entityType,
+    getEntityId
+}: {
+    entityType: CommentEntityType,
+    getEntityId: (req: Request) => number
+}) {
     return (req: Request, res: Response) => {
         // TODO
 
-        console.log("Inserting comments for entity: ", entity, " with id: ", req.params.entityId)
+        console.log("Inserting comments for entity: ", entityType, " with id: ", req.params.entityId)
 
         throw "Not implemented yet"
     }
