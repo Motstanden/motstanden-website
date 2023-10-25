4e8de7d19dfac0a0fbecd5b6fbf7a07411b440ba
Torstein Wilhelmsen Sandvik <torsteinws@gmail.com>
2023-10-25 19:16:42 +0200

    Create like entity type

--- common/src/enums/index.ts
+++ common/src/enums/index.ts
@@ -1,9 +1,9 @@
 export * from "./Clef.js"
 export * from "./CommentEntityType.js"
+export * from "./LikeEntityType.js"
 export * from "./ParticipationStatus.js"
 export * from "./Tone.js"
 export * from "./UserEditMode.js"
 export * from "./UserGroup.js"
 export * from "./UserRank.js"
 export * from "./UserStatus.js"
-
