480fcff55151c940828d1adf3f1c3db5ce877966
Torstein Wilhelmsen Sandvik <git@torsteinws.no>
2024-01-19 09:12:26 +0700

    [Refactor] Move AppBar to separate file

---
+++ client/src/layout/AppBar/AppBar.tsx
@@ -0,0 +1,118 @@
+import MenuIcon from '@mui/icons-material/Menu';
+import { AppBar as MuiAppBar, Box, Divider, IconButton, Link, Stack, SxProps, Theme, Toolbar, Typography, useMediaQuery } from "@mui/material";
+import { Link as RouterLink } from "react-router-dom";
+import MotstandenImg from "src/assets/logos/motstanden.png";
+import { useAuth } from 'src/context/Authentication';
+import { ThemeSwitchButton } from 'src/layout/ThemeSwitchButton';
+import UserAvatar from 'src/layout/UserAvatar';
+
+export function AppBar({ 
+    onMenuClick, 
+    sx,
+    position = "fixed"
+}: { 
+    onMenuClick?: VoidFunction,
+    sx?: SxProps,
+    position?: "fixed" | "absolute" | "sticky" | "static" | "relative" 
+}) {
+
+    const isMediumScreen = useMediaQuery((theme: Theme) => theme.breakpoints.down('md'));
+    const isSmallScreen = useMediaQuery("(max-width: 663px)");
+    const isMobileScreen = useMediaQuery((theme: Theme) => theme.breakpoints.down('sm'));
+
+    return (
+        <MuiAppBar position={position} sx={sx}>
+            <Toolbar sx={{
+                bgcolor: "primary.main",
+                height: "100%",
+                justifyContent: "space-between",
+            }}>
+                <IconButton
+                    sx={{ display: { sm: 'none' }, }}
+                    onClick={onMenuClick}>
+                    <MenuIcon sx={{ color: "primary.contrastText" }} />
+                </IconButton>
+                <Stack
+                    direction="row"
+                    alignItems="center"
+                    gap="15px"
+                >
+                    <Box
+                        component={RouterLink}
+                        to="/"
+                        sx={{ display: isSmallScreen ? "none" : "flex" }}>
+                        <img
+                            src={MotstandenImg}
+                            style={{ height: "57px" }}
+                            loading="lazy" />
+                    </Box>
+                    <Typography
+                        component={RouterLink}
+                        to="/"
+                        noWrap
+                        variant={isMobileScreen ? "inherit" : "h5"}
+                        sx={{
+                            fontWeight: 700,
+                            letterSpacing: '.1rem',
+                            color: 'inherit',
+                            textDecoration: 'none',
+                        }}
+                    >
+                        {isMediumScreen ? "MOTSTANDEN" : "Den Ohmske Motstanden"}
+                    </Typography>
+                </Stack>
+                <Stack
+                    direction="row"
+                    alignItems="center"
+                    color="primary.contrastText"
+                    style={{ height: "100%" }}
+                >
+                    <ThemeSwitchButton
+                        fontSize='large'
+                        sx={{
+                            width: "42px",
+                            height: "42px",
+                            display: { xs: "none", sm: "flex" },
+                            color: "inherit"
+                        }} />
+                    <Divider
+                        light={true}
+                        orientation='vertical'
+                        flexItem variant='middle'
+                        color="inherit"
+                        sx={{
+                            mx: 0.5,
+                            height: "50%",
+                            my: "auto",
+                            bgcolor: "primary.contrastText",
+                            opacity: 0.3,
+                            display: { xs: "none", sm: "flex" }
+                        }} />
+                    <UserInfo />
+                </Stack>
+            </Toolbar>
+        </MuiAppBar>
+    );
+}
+
+function UserInfo() {
+    const auth = useAuth()
+
+    if(auth.user)
+        return <UserAvatar/>
+
+    return (
+        <Link
+            component={RouterLink}
+            to="/logg-inn"
+            underline="hover"
+            sx={{
+                color: "inherit",
+                pl: 1,
+                fontWeight: 600
+            }}
+        >
+            LOGG INN
+        </Link>
+    )
+}
--- client/src/layout/AppLayout.tsx
+++ client/src/layout/AppLayout.tsx
@@ -1,13 +1,9 @@
-import MenuIcon from '@mui/icons-material/Menu';
-import { AppBar, Box, Divider, IconButton, Link, Stack, Theme, Toolbar, Typography, useMediaQuery } from "@mui/material";
+import { Box, Theme, useMediaQuery } from "@mui/material";
 import { useState } from "react";
-import { Outlet, Link as RouterLink } from "react-router-dom";
-import MotstandenImg from "src/assets/logos/motstanden.png";
-import { useAuth } from "src/context/Authentication";
+import { Outlet } from "react-router-dom";
 import { FooterContent } from "src/layout/Footer";
 import { SideDrawer } from 'src/layout/SideDrawer/SideDrawer';
-import { ThemeSwitchButton } from 'src/layout/ThemeSwitchButton';
-import UserAvatar from 'src/layout/UserAvatar';
+import { AppBar } from "./AppBar/AppBar";
 
 const largeDrawerWidth = 290
 const mediumDrawerWidth = 220
@@ -38,15 +34,19 @@ export function AppLayout() {
 
     return (
         <div style={{display: "flex"}}>
-            <AppBar
-                position={isSmallScreen ? "fixed" : "absolute"}
-                sx={{
-                    width: { sm: `calc(100% - ${drawerWidth}px)` },
-                    ml: { sm: `${drawerWidth}px` },
-                    height: `${appBarHeight}px`,
-                }}>
-                <AppBarContent onMenuClick={toggleDrawer}/>
-            </AppBar>
+            
+            <header>
+                <AppBar 
+                    onMenuClick={toggleDrawer}
+                    position={isSmallScreen ? "fixed" : "absolute"}
+                    sx={{
+                        width: { sm: `calc(100% - ${drawerWidth}px)` },
+                        ml: { sm: `${drawerWidth}px` },
+                        height: `${appBarHeight}px`,
+                    }}
+                />
+            </header>
+
             <Box component="nav" 
                 sx={{ 
                     flexShrink: { sm: 0 },
@@ -61,6 +61,7 @@ export function AppLayout() {
                     headerHeight={appBarHeight}
                 />
             </Box>
+
             <Box sx={{ 
                 flexGrow: 1, 
                 pt: `${appBarHeight}px`,  
@@ -74,108 +75,5 @@ export function AppLayout() {
                 </footer>
             </Box>
         </div>
-    )
-}
-
-function AppBarContent( {onMenuClick}: {onMenuClick?: VoidFunction}) {
-
-    const isMediumScreen = useMediaQuery((theme: Theme) => theme.breakpoints.down('md'));
-    const isSmallScreen = useMediaQuery("(max-width: 663px)");
-    const isMobileScreen = useMediaQuery((theme: Theme) => theme.breakpoints.down('sm'))
-
-    return (
-        <Toolbar sx={{
-            bgcolor: "primary.main",
-            height: "100%",
-            justifyContent: "space-between",
-        }}>
-            <IconButton 
-                sx={{display: { sm: 'none' },}}
-                onClick={onMenuClick}>
-                <MenuIcon sx={{ color: "primary.contrastText" }}  />
-            </IconButton>
-            <Stack 
-                direction="row" 
-                alignItems="center" 
-                gap="15px"
-            >
-                <Box
-                    component={RouterLink}
-                    to="/"
-                    sx={{ display: isSmallScreen ? "none" : "flex" }}>
-                    <img 
-                        src={MotstandenImg} 
-                        style={{ height: "57px"}} 
-                        loading="lazy" />
-                </Box>
-                <Typography
-                    component={RouterLink}
-                    to="/"
-                    noWrap
-                    variant={isMobileScreen ? "inherit" : "h5"}
-                    sx={{
-                        fontWeight: 700,
-                        letterSpacing: '.1rem',
-                        color: 'inherit',
-                        textDecoration: 'none',
-                    }}
-                >
-                    {isMediumScreen ? "MOTSTANDEN" : "Den Ohmske Motstanden"}
-                </Typography>
-            </Stack>
-            <Stack 
-                direction="row" 
-                alignItems="center" 
-                color="primary.contrastText"
-                style={{height: "100%"}}
-                >
-                <ThemeSwitchButton 
-                    fontSize='large'
-                    sx={{
-                        width: "42px", 
-                        height: "42px", 
-                        display: {xs: "none", sm: "flex"},
-                        color: "inherit"
-                    }}
-                />
-                <Divider 
-                    light={true}
-                    orientation='vertical'  
-                    flexItem variant='middle'
-                    color="inherit"
-                    sx={{
-                        mx: 0.5,
-                        height: "50%",
-                        my: "auto",
-                        bgcolor: "primary.contrastText",
-                        opacity: 0.3,
-                        display: {xs: "none", sm: "flex"}
-                    }}
-                    />
-                <UserInfo/>
-            </Stack>
-        </Toolbar>
-    )
-}
-
-function UserInfo() {
-    const auth = useAuth()
-
-    if(auth.user)
-        return <UserAvatar/>
-
-    return (
-        <Link
-            component={RouterLink}
-            to="/logg-inn"
-            underline="hover"
-            sx={{
-                color: "inherit",
-                pl: 1,
-                fontWeight: 600
-            }}
-        >
-            LOGG INN
-        </Link>
     )
 }
