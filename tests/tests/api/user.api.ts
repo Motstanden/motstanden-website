42ff65a497b111f1bf67354a103873b06907d07d
Torstein Wilhelmsen Sandvik <git@torsteinws.no>
2024-01-31 20:58:35 +0800

    Add more api tests

--- tests/tests/api/user.api.ts
+++ tests/tests/api/user.api.ts
@@ -1,4 +1,4 @@
-import { expect, test } from '@playwright/test'
+import { APIRequest, APIRequestContext, expect, test } from '@playwright/test'
 import { UserGroup } from 'common/enums'
 import { apiLogIn, getUser } from '../../utils/auth.js'
 
@@ -11,12 +11,33 @@ test.describe("GET requests sends correct types", () => {
     })
 
     test("GET /member-list", async ({request}) => {
-        const res = await request.get("/api/member-list")
+        const res = await getMemberList(request)
         expect(res.ok()).toBeTruthy()
     })
 
     test("GET /simplified-member-list", async ({request}) => {
-        const res = await request.get("/api/simplified-member-list")
+        const res = await getMemberList(request)
         expect(res.ok()).toBeTruthy()
     })
 })
+
+test.describe("GET refuse unauthenticated requests", () => { 
+
+    test("GET /member-list", async ({request}) => { 
+        const res = await getMemberList(request)
+        expect(res.ok()).toBeFalsy()
+    })
+
+    test("GET /simplified-member-list", async ({request}) => { 
+        const res = await getSimplifiedMemberList(request)
+        expect(res.ok()).toBeFalsy()
+    })
+})
+
+async function getMemberList(request: APIRequestContext) {
+    return await request.get("/api/member-list")
+}
+
+async function getSimplifiedMemberList(request: APIRequestContext) { 
+    return await request.get("/api/simplified-member-list")
+}
