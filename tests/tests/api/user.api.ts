6720452ea7da3fd251d6670aade9a7a51282f9c6
Torstein Wilhelmsen Sandvik <git@torsteinws.no>
2024-01-31 23:50:53 +0800

    Use zod to validate users from api

--- tests/tests/api/user.api.ts
+++ tests/tests/api/user.api.ts
@@ -1,7 +1,37 @@
-import { APIRequest, APIRequestContext, expect, test } from '@playwright/test'
-import { UserGroup } from 'common/enums'
+import { APIRequestContext, expect, test } from '@playwright/test'
+import { UserGroup, UserRank, UserStatus } from 'common/enums'
+import { z } from "zod"
 import { apiLogIn, getUser } from '../../utils/auth.js'
 
+const userSchema = z.object({ 
+    firstName: z.string(),
+    middleName: z.string(),
+    lastName: z.string(),
+    email: z.string().email(),
+    profilePicture: z.string(),
+    id: z.number(),
+    groupId: z.number(),
+    groupName: z.nativeEnum(UserGroup),
+    rank: z.nativeEnum(UserRank),
+    capeName: z.string(),
+    status: z.nativeEnum(UserStatus),
+    phoneNumber: z.number().min(10000000).max(99999999).nullable(),
+    birthDate: z.coerce.date().nullable(),
+    startDate: z.coerce.date(),
+    endDate: z.coerce.date().nullable(),
+    createdAt: z.coerce.date(),
+    updatedAt: z.coerce.date(),
+})
+
+const userArraySchema = z.array(userSchema)
+
+const simplifiedUserSchema = z.object({
+    id: z.number(),
+    fullName: z.string(),
+    initials: z.string().min(2).max(3),
+})
+
+const simplifiedUserArraySchema = z.array(simplifiedUserSchema)
 
 test.describe("GET requests sends correct types", () => {
 
@@ -13,11 +43,17 @@ test.describe("GET requests sends correct types", () => {
     test("GET /member-list", async ({request}) => {
         const res = await getMemberList(request)
         expect(res.ok()).toBeTruthy()
+
+        const users = await res.json()
+        expect( () => userArraySchema.parse(users)).not.toThrow()
     })
 
     test("GET /simplified-member-list", async ({request}) => {
-        const res = await getMemberList(request)
+        const res = await getSimplifiedMemberList(request)
         expect(res.ok()).toBeTruthy()
+
+        const users = await res.json()
+        expect(() => simplifiedUserArraySchema.parse(users)).not.toThrow()
     })
 })
 
