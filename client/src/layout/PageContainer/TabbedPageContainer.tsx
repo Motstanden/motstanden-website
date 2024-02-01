25cd343f059581f834f888de5a08bf324cf4033f
Torstein Wilhelmsen Sandvik <git@torsteinws.no>
2024-02-01 13:44:01 +0800

    Move PageTab into TabbedPageContainer

--- client/src/layout/PageContainer/TabbedPageContainer.tsx
+++ client/src/layout/PageContainer/TabbedPageContainer.tsx
@@ -1,9 +1,10 @@
-import { useMediaQuery } from "@mui/material";
+import { Divider, Tab, Tabs, useMediaQuery } from "@mui/material";
 import React, { useEffect } from "react";
-import { PageTab, PageTabItem } from "src/components/PageTab";
+import { Link as RouterLink, Location as RouterLocation, matchPath, useLocation } from "react-router-dom";
 import { useAppBarStyle } from "src/context/AppBarStyle";
 import { useAppTheme } from "src/context/Themes";
 import { PageContainer } from "./PageContainer";
+
 
 export const tabBarHeight = 48
 
@@ -34,29 +35,42 @@ export function TabbedPageContainer({
         }
     }, [isMobile]);
 
-    const tabPositionStyle: React.CSSProperties = isMobile ? {} : {
-        position: "fixed",
-        width: "100%",
-        zIndex: 1000,
-    };
+    // Select current tab based on url
+    const location = useLocation()
+    const currentTab = findActiveTab(tabItems, location, {matchChildPath: matchChildPath}) ?? tabItems[0]
 
     return (
         <>
-            <PageTab
-                items={tabItems}
-                matchChildPath={matchChildPath}
-                style={{
-                    backgroundColor: theme.palette.background.paper,
-                    ...tabPositionStyle,
-                }}
-                tabProps={{
-                    style: {
+            <div style={ isMobile ? undefined : {
+                    position: "fixed",
+                    width: "100%",
+                    zIndex: 1000,
+                }}>
+                <Tabs
+                    value={currentTab.label}
+                    textColor="secondary"
+                    indicatorColor="secondary"
+                    variant={isMobile ? "fullWidth" : "standard"}
+                    scrollButtons="auto"
+                    sx={{
                         height: `${tabBarHeight}px`,
-                    },
-                    sx: {
                         boxShadow: isMobile ? undefined : appBar.boxShadowValue,
-                    }
-                }} />
+                        backgroundColor: theme.palette.background.paper,
+                    }}
+                >
+                    {tabItems.map(item => (
+                        <Tab
+                            key={item.label}
+                            component={RouterLink}
+                            to={item.to}
+                            value={item.label}
+                            label={item.label}
+                            wrapped
+                        />)
+                    )}
+                </Tabs>
+                <Divider/>
+            </div>
             <div style={{
                 paddingTop: isMobile ? undefined : `${tabBarHeight}px`
             }}>
@@ -66,4 +80,27 @@ export function TabbedPageContainer({
             </div>
         </>
     );
+}
+
+function findActiveTab(
+    tabs: PageTabItem[], 
+    location: RouterLocation,
+    options?: {
+        matchChildPath?: boolean
+    }
+): PageTabItem | undefined {
+    for(let i = 0; i < tabs.length; i++) {
+        const item = tabs[i]
+        const pattern = options?.matchChildPath ? item.to + "/*" : item.to
+        const isMatch = !!matchPath(pattern, location.pathname)
+        if(isMatch) {
+            return item
+        }
+    }
+    return undefined;
+}
+
+interface PageTabItem {
+    to: string,
+    label: string
 }
