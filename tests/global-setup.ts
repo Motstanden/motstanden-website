d5b09cdf9b042582c53b4d640a2cbbf103b88eb6
Torstein Wilhelmsen Sandvik <git@torsteinws.no>
2024-02-10 12:27:22 +0800

    Make sure all tests uses system timezone

--- tests/global-setup.ts
+++ tests/global-setup.ts
@@ -1,4 +1,4 @@
-import { FullConfig, request } from "@playwright/test";
+import { FullConfig, firefox, request } from "@playwright/test";
 import { UserGroup } from "common/enums";
 import { testUserVariationsCount, unsafeApiLogIn, unsafeGetUser } from "./utils/auth.js";
 
@@ -8,17 +8,17 @@ export default async function globalSetup(config: FullConfig) {
 }
 
 async function authSetup() {
-    console.log("[Setup] Authenticating users...")
+    await setupBaseStorageState()
 
+    console.log("[Setup] Authenticating users...")
+    
     const groups = Object.values(UserGroup)
     const loginFunctions: Array<Promise<void>> = []
-    
     for(let i = 0; i < groups.length; i++) {
         for(let j = 0; j < testUserVariationsCount; j++) {
             loginFunctions.push(loginUser(groups[i], j))
         }
     }
-
     await Promise.all(loginFunctions)
 
     console.log("[Setup] Done")
@@ -26,8 +26,24 @@ async function authSetup() {
 
 async function loginUser(userGroup: UserGroup, variantIndex: number) {
     const user = unsafeGetUser(userGroup, variantIndex)
-    const apiContext = await request.newContext({ baseURL: process.env.BASEURL })
+    const apiContext = await request.newContext({ baseURL: process.env.BASEURL, storageState: baseStorageState })
     await unsafeApiLogIn(apiContext, user.email)
     apiContext.storageState({ path: user.storageStatePath })
     apiContext.dispose()
+}
+
+const baseStorageState = "storage-state/base.json"
+
+async function setupBaseStorageState() {
+    console.log("[Setup] Setting time zone to match system time zone...")
+
+    const browser = await firefox.launch()
+    const page = await browser.newPage({ baseURL: process.env.BASEURL })
+    await page.goto("/")
+
+    await page.getByRole('button', { name: 'Innstillinger' }).click();
+    await page.getByRole('button', { name: 'System' }).nth(1).click();
+    await page.context().storageState({ path: baseStorageState })
+
+    await browser.close()
 }
