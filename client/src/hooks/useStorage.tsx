0dc047779da4d0d870f93abfe605c469817a7662
Torstein Wilhelmsen Sandvik <git@torsteinws.no>
2024-02-26 23:50:52 +0700

    Create hook that persists state in either local storage or session storage

---
+++ client/src/hooks/useStorage.tsx
@@ -0,0 +1,52 @@
+import { useState } from "react";
+
+type StorageType<T> = [
+    T, 
+    (value: T | (() => T)) => void, 
+    VoidFunction
+];
+
+// A custom hook that uses either localStorage or sessionStorage to store and retrieve data
+function useStorage<T>(key: string, initialValue: T | (() => T), storage: Storage): StorageType<T> {
+
+    // Get the stored value from the storage or use the initial value
+    const [storedValue, setStoredValue] = useState<T>(() => {
+        const item = storage.getItem(key);
+
+        if (item !== null)
+            return JSON.parse(item);
+
+        if (initialValue instanceof Function)
+            return initialValue();
+
+        return initialValue;
+    });
+
+    // Define a function that updates the stored value and the storage
+    const setValue = (newValue: T | (() => T)) => {
+        const value = newValue instanceof Function ? newValue() : newValue;
+        setStoredValue(value);
+        storage.setItem(key, JSON.stringify(value));
+    };
+
+    const clearStorage = () => {
+        storage.removeItem(key);
+    }
+
+    // Return the stored value and the setter function
+    return [storedValue, setValue, clearStorage];
+}
+
+/**
+ * A custom hook that uses localStorage to store and retrieve data. Works like useState but persists the state in localStorage.
+ */
+export function useLocalStorage<T>(key: string, initialValue: T | (() => T)): StorageType<T> {
+    return useStorage(key, initialValue, window.localStorage);
+}
+
+/**
+ * A custom hook that uses sessionStorage to store and retrieve data. Works like useState but persists the state in sessionStorage.
+ */
+export function useSessionStorage<T>(key: string, initialValue: T | (() => T)): StorageType<T> {
+    return useStorage(key, initialValue, window.sessionStorage);
+}
