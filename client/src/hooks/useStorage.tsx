3c8ac068858c385569c8a460ecdfaf2abeccfe30
Torstein Wilhelmsen Sandvik <git@torsteinws.no>
2024-02-27 01:17:32 +0700

    [Refactor] Create InitialValueType

--- client/src/hooks/useStorage.tsx
+++ client/src/hooks/useStorage.tsx
@@ -1,14 +1,20 @@
 import { useState } from "react";
 
+type InitialValueType<T> = T | (() => T);
+
 type StorageType<T> = [
     T, 
-    (value: T | (() => T)) => void, 
+    (value: InitialValueType<T>) => void, 
     VoidFunction
 ];
 
 // A custom hook that uses either localStorage or sessionStorage to store and retrieve data
-function useStorage<T>(key: string, initialValue: T | (() => T), storage: Storage): StorageType<T> {
-
+function useStorage<T>(
+    key: string, 
+    initialValue: InitialValueType<T>, 
+    storage: Storage
+): StorageType<T> {
+    
     // Get the stored value from the storage or use the initial value
     const [storedValue, setStoredValue] = useState<T>(() => {
         const item = storage.getItem(key);
@@ -40,13 +46,19 @@ function useStorage<T>(key: string, initialValue: T | (() => T), storage: Storag
 /**
  * A custom hook that uses localStorage to store and retrieve data. Works like useState but persists the state in localStorage.
  */
-export function useLocalStorage<T>(key: string, initialValue: T | (() => T)): StorageType<T> {
+export function useLocalStorage<T>(
+    key: string, 
+    initialValue: InitialValueType<T>
+): StorageType<T> {
     return useStorage(key, initialValue, window.localStorage);
 }
 
 /**
  * A custom hook that uses sessionStorage to store and retrieve data. Works like useState but persists the state in sessionStorage.
  */
-export function useSessionStorage<T>(key: string, initialValue: T | (() => T)): StorageType<T> {
+export function useSessionStorage<T>(
+    key: string, 
+    initialValue: InitialValueType<T>
+): StorageType<T> {
     return useStorage(key, initialValue, window.sessionStorage);
 }
