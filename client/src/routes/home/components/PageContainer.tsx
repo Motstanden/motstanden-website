725c44e1584c96abdeeec321775f2e0df361b1c4
Torstein Wilhelmsen Sandvik <git@torsteinws.no>
2024-03-04 18:45:10 +0700

    Create context for unread wall posts

--- client/src/routes/home/components/PageContainer.tsx
+++ client/src/routes/home/components/PageContainer.tsx
@@ -1,16 +1,39 @@
+import { Badge } from "@mui/material";
 import { Outlet } from "react-router-dom";
 import { TabbedPageContainer } from "src/layout/PageContainer/TabbedPageContainer";
+import { UnreadWallPostsProvider, useUnreadWallPosts } from "../context/UnreadWallPosts";
 
-export default function PageContainer({ children }: { children?: React.ReactNode }) { 
+export default function PageContainer() { 
     return (
-        <TabbedPageContainer
-            tabItems={[
-                { to: "/hjem", label: "Hjem" },
-                { to: "/vegg", label: "Vegg"}
-            ]}
-            matchChildPath={true}
-        >
-            <Outlet/>
-        </TabbedPageContainer>
+        <UnreadWallPostsProvider>
+            <TabbedPageContainer
+                tabItems={[
+                    { to: "/hjem", label: "Hjem" },
+                    { to: "/vegg", label: <WallLabel/>}
+                ]}
+                matchChildPath={true}
+            >
+                <Outlet/>
+            </TabbedPageContainer>
+        </UnreadWallPostsProvider>
+    )
+}
+
+function WallLabel(){
+    const { unreadPosts } = useUnreadWallPosts()
+    return (
+        <Badge 
+            badgeContent={unreadPosts}
+            color="secondary"
+            max={99}
+            sx={{
+                ".MuiBadge-badge": {
+                    right: -20,
+                    top: "50%",
+                }
+            }}
+            >
+            Vegg
+        </Badge>
     )
 }
