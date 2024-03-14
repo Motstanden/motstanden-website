80309a574abd415f32aa5a8bfbd1442b61cfc239
Torstein Wilhelmsen Sandvik <git@torsteinws.no>
2024-03-14 18:25:10 +0700

    [WIP] Convert documents to typescript

--- server/src/api/documents.ts
+++ server/src/api/documents.ts
@@ -3,13 +3,21 @@ import express from "express";
 import { dbReadOnlyConfig, motstandenDB } from "../config/databaseConfig.js";
 import { AuthenticateUser } from "../middleware/jwtAuthenticate.js";
 
-let router = express.Router()
+const router = express.Router()
 
 router.get("/documents",
     AuthenticateUser({ failureRedirect: "documents_public" }),
     (req, res) => {
         const db = new Database(motstandenDB, dbReadOnlyConfig)
-        const stmt = db.prepare("SELECT title, filename AS url FROM document ORDER BY document_id DESC")
+        const stmt = db.prepare(`
+            SELECT 
+                title, 
+                filename AS url 
+            FROM 
+                document 
+            ORDER BY 
+                document_id DESC
+            `)
         const documents = stmt.all();
         res.send(documents)
         db.close()
@@ -18,15 +26,17 @@ router.get("/documents",
 
 router.get("/documents_public", (req, res) => {
     const db = new Database(motstandenDB, dbReadOnlyConfig)
-    const stmt = db.prepare("SELECT \
-                                title, \
-                                filename AS url \
-                            FROM \
-                                document \
-                            WHERE \
-                                is_public=1\
-                            ORDER BY \
-                                document_id DESC")
+    const stmt = db.prepare(`
+        SELECT 
+            title, 
+            filename AS url 
+        FROM 
+            document 
+        WHERE 
+            is_public=1
+        ORDER BY 
+            document_id DESC`
+        )
     const documents = stmt.all();
     res.send(documents)
     db.close()
