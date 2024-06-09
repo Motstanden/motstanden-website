4154301c36d97166b3866a6488d48df9ecc3b0cf
Torstein Wilhelmsen Sandvik <git@torsteinws.no>
2024-06-09 20:34:55 +0700

    Clear unread wall posts when the url is /vegg

--- client/src/routes/home/Wall.tsx
+++ client/src/routes/home/Wall.tsx
@@ -1,23 +1,9 @@
 import { PostingWall } from "src/components/wall/PostingWall"
 import { useTitle } from "src/hooks/useTitle"
-import { useUnreadWallPosts } from "./context/UnreadWallPosts"
 
 export default function WallPage() {
     useTitle("Vegg")
-
-    const { clearUnreadPosts } = useUnreadWallPosts()
-
-    const onPostSuccess = () => {
-        clearUnreadPosts({refetchData: true})
-    }
-
-    const onLoadedPosts = () => {
-        clearUnreadPosts({refetchData: false})
-    }
-
     return (
-        <>
-            <PostingWall onPostSuccess={onPostSuccess} onLoadedPosts={onLoadedPosts}/>
-        </>
+        <PostingWall/>
     )
 } 
--- client/src/routes/home/components/PageContainer.tsx
+++ client/src/routes/home/components/PageContainer.tsx
@@ -1,38 +1,80 @@
 import { Badge } from "@mui/material";
-import { Outlet } from "react-router-dom";
-import { TabbedPageContainer } from "src/layout/PageContainer/TabbedPageContainer";
-import { UnreadWallPostsProvider, useUnreadWallPosts } from "../context/UnreadWallPosts";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
+import { Count } from "common/interfaces";
+import { useState } from "react";
+import { Outlet, useMatch } from "react-router-dom";
 import { useAppBarHeader } from "src/context/AppBarHeader";
+import { useDebounce } from "src/hooks/useDebounce";
+import { TabbedPageContainer } from "src/layout/PageContainer/TabbedPageContainer";
+import { fetchFn } from "src/utils/fetchAsync";
 
 export default function PageContainer() { 
     useAppBarHeader("Hjem")
+
+    const { unreadCount, isUpdating, clearUnreadCount } = useUnreadWallPosts()
+
+    const isWallPage = !!useMatch("/vegg")
+
+    useDebounce( async () => {
+        if(unreadCount > 0 && !isUpdating && isWallPage) {
+            await clearUnreadCount()
+        }
+    }, 1000, [unreadCount, isUpdating, isWallPage])
+
     return (
-        <UnreadWallPostsProvider>
-            <TabbedPageContainer
-                tabItems={[
-                    { to: "/hjem", label: "Oversikt" },
-                    { to: "/vegg", label: <WallLabel/>}
-                ]}
-                matchChildPath={true}
-            >
-                <Outlet/>
-            </TabbedPageContainer>
-        </UnreadWallPostsProvider>
+        <TabbedPageContainer
+            tabItems={[
+                { to: "/hjem", label: "Oversikt" },
+                { to: "/vegg", label: <WallLabel unreadPostsCount={unreadCount}/>}
+            ]}
+            matchChildPath={true}
+        >
+            <Outlet/>
+        </TabbedPageContainer>
     )
 }
 
-function WallLabel(){
-    const { unreadPosts } = useUnreadWallPosts()
+function useUnreadWallPosts() {
 
-    if (unreadPosts <= 0)
+    const queryKey = ["wall-posts", "unread", "count"]
+    const { data, isPending, isError } = useQuery<Count>({
+        queryKey: queryKey,
+        queryFn: fetchFn("/api/wall-posts/unread/count"),
+    })
+    const unreadCount = data?.count ?? 0
+    const isSuccess = !isPending && !isError
+
+    const queryClient = useQueryClient()
+    
+    const [isPosting, setIsPosting] = useState(false)
+
+    const clearUnreadCount = async () => { 
+        if(isSuccess && unreadCount > 0 && !isPosting) {
+            setIsPosting(true)
+            await fetch("/api/wall-posts/unread/count/reset", { method: "POST" })
+            await queryClient.invalidateQueries({queryKey: queryKey})
+            setIsPosting(false)
+        }
+    }
+
+    return {
+        unreadCount,
+        isUpdating: isPosting || !isSuccess,
+        clearUnreadCount,
+    }
+}
+
+function WallLabel( {unreadPostsCount}: {unreadPostsCount: number}){
+
+    if (unreadPostsCount <= 0)
         return "Vegg"
 
     let positionRight: number
     let marginRight: number
-    if(unreadPosts < 10) {
+    if(unreadPostsCount < 10) {
         positionRight = 20
         marginRight = positionRight + 12
-    } else if (unreadPosts < 100) {
+    } else if (unreadPostsCount < 100) {
         positionRight = 23
         marginRight = positionRight + 14.5
     } else {
@@ -42,7 +84,7 @@ function WallLabel(){
 
     return (
         <Badge 
-            badgeContent={unreadPosts}
+            badgeContent={unreadPostsCount}
             color="secondary"
             max={99}
             sx={{
