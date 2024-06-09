ad5653d0dd5ad8125fcb7572c34eb66d258034a4
Torstein Wilhelmsen Sandvik <git@torsteinws.no>
2024-06-10 02:50:27 +0700

    Remove obsolete interface

--- common/src/interfaces/User.ts
+++ common/src/interfaces/User.ts
@@ -34,9 +34,3 @@ export interface User extends NewUser {
     createdAt: string;              // Format: 'YYYY-MM-DD HH-MM-SS'
     updatedAt: string;              // Format: 'YYYY-MM-DD HH-MM-SS'
 }
-
-// The structure of the user data that is stored in a 'unsafe' cookie.
-// By unsafe, I mean that the data in the cookie can be modified by the client.
-export interface UnsafeUserCookie extends User {
-    expires: Date;                // Format: Seconds since epoch
-}
