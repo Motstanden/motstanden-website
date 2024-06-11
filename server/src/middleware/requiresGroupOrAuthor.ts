ddbd642248c02a694bd5a42d8fb5cedfbd51e2dd
Torstein Wilhelmsen Sandvik <git@torsteinws.no>
2024-06-12 01:43:30 +0700

    Fix potential security issue

--- server/src/middleware/requiresGroupOrAuthor.ts
+++ server/src/middleware/requiresGroupOrAuthor.ts
@@ -5,8 +5,8 @@ import { hasGroupAccess } from "../utils/accessTokenUtils.js"
 
 interface requiresGroupOrAuthorProps {
     requiredGroup: UserGroup,
-    getId: (req: Request) => number | unknown | undefined,
-    getAuthorInfo: (id: number) => AuthoredItem    
+    getId: (req: Request) => number | undefined,
+    getAuthorInfo: (id: number) => AuthoredItem | undefined   
 }
 
 export function requiresGroupOrAuthor(options: requiresGroupOrAuthorProps) {
@@ -23,20 +23,32 @@ function authenticatePermission(
     { requiredGroup, getId, getAuthorInfo }: requiresGroupOrAuthorProps
 ) {
 
-    // Check if the posted quoteId is valid
-    let id: number | unknown | undefined = getId(req)
+    // Check if the id is a number
+    let id: unknown = getId(req)
     if (!id || typeof id !== "number") {
         return res.status(400).send("Bad data")
     }
-    let item: AuthoredItem
+
+    // Get current user
+    const user = req.user as AccessTokenData | undefined
+    if(user?.userId === undefined) {
+        // This should never happen, but if it does, it's a critical error!
+        console.error(`PANIC!! This url requires authentication but the user is not authenticated.\n\tUrl: ${req.url}`)
+        return res.status(401).send("Unauthorized")
+    }
+
+    // Get the item from the database
+    let item: AuthoredItem | undefined
     try {
         item = getAuthorInfo(id)
     } catch {
-        return res.status(400).send("bad data")
+        return res.status(400).send("Something went wrong")
     }
+    if(item?.createdBy === undefined)
+        return res.status(404).send("Item not found")
 
-    // Allow quote to be modified if the user is admin or is original author
-    const user = req.user as AccessTokenData
+
+    // Allow access if the user has the right privileges or is the author of the item
     const hasPrivilege = hasGroupAccess(user, requiredGroup)
     const isAuthor = item.createdBy === user.userId
     if (hasPrivilege || isAuthor) {
