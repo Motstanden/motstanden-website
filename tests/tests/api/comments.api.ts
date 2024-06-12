6d958d6b7ed162f838f21b26373fafcef31017fd
Torstein Wilhelmsen Sandvik <git@torsteinws.no>
2024-06-12 11:29:05 +0700

    Test that comments increases unread counter [passing]

--- tests/tests/api/comments.api.ts
+++ tests/tests/api/comments.api.ts
@@ -1,7 +1,8 @@
-import { UserGroup } from "common/enums"
+import { CommentEntityType, UserGroup } from "common/enums"
 import { testUserVariationsCount, unsafeGetUser } from "../../utils/auth.js"
 import { APIRequestContext, expect, request, test } from '@playwright/test'
-import { Count } from "common/interfaces"
+import { Count, NewComment } from "common/interfaces"
+import { randomString } from "../../utils/randomString.js"
 
 test.describe.serial("Comments test suite", () => {
     const user1 = unsafeGetUser(UserGroup.Contributor, testUserVariationsCount - 1)
@@ -9,6 +10,13 @@ test.describe.serial("Comments test suite", () => {
     
     let api1: APIRequestContext
     let api2: APIRequestContext
+
+    let unreadComments: { 
+        entityId: number,
+        entityType: CommentEntityType,
+        comment: string
+    }[] = []
+    let unreadCount = 0
 
     test.beforeEach( async () => {
         api1 = await request.newContext( { storageState: user1.storageStatePath } )
@@ -20,7 +28,7 @@ test.describe.serial("Comments test suite", () => {
         api2.dispose()
     })
 
-    test("Initial unread comments count is 0", async ({ }) => {
+    test("Initial unread comments count is 0", async () => {
         await resetUnreadCount(api1)
         await resetUnreadCount(api2)
 
@@ -30,6 +38,27 @@ test.describe.serial("Comments test suite", () => {
         expect(count1).toBe(0)
         expect(count2).toBe(0)
     })
+
+    const testCommentIncreasesCounter = async (entityType: CommentEntityType) => {
+        const entityId = 1
+        const comment = randomString(`Comment on ${entityType} ${entityId}`)
+
+        await createComment(api1, entityType, entityId, comment)
+
+        const count1 = await getUnreadCount(api1)
+        const count2 = await getUnreadCount(api2)
+
+        expect(count1).toBe(0)
+        expect(count2).toBe(unreadCount + 1)
+
+        unreadComments.push({entityId, entityType, comment})
+        unreadCount++
+    }
+
+    test("Event comment increases unread count", async () => testCommentIncreasesCounter(CommentEntityType.Event))
+    test("Poll comment increases unread count", async () => testCommentIncreasesCounter(CommentEntityType.Poll))
+    test("Song lyric comment increases unread count", async () => testCommentIncreasesCounter(CommentEntityType.SongLyric))
+    test("Wall post comment increases unread count", async () => testCommentIncreasesCounter(CommentEntityType.WallPost))
 })
 
 async function getUnreadCount(api:APIRequestContext) {
@@ -44,4 +73,14 @@ async function resetUnreadCount(api: APIRequestContext) {
     const res = await api.post("/api/comments/unread/count/reset")
     if(!res.ok())
         throw new Error("Failed to reset unread count")
+}
+
+async function createComment(api: APIRequestContext, entityType: CommentEntityType, entityId: number, comment: string, ) { 
+    const data: NewComment = { 
+        comment: comment 
+    }
+    const res = await api.post(`/api/${entityType}/${entityId}/comments/new`, {data: data})
+    if(!res.ok()) {
+        throw new Error(`Failed to create ${entityType} comment.\n${await res.text()}`)
+    }
 }
