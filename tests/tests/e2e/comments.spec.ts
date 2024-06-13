78f18b6700ca1bfafa5e3b1695c135e76da22c20
Torstein Wilhelmsen Sandvik <git@torsteinws.no>
2024-06-13 13:02:22 +0700

    Write test for deleting comments [failing]

--- tests/tests/e2e/comments.spec.ts
+++ tests/tests/e2e/comments.spec.ts
@@ -1,30 +1,62 @@
-import { Page, expect, test } from "@playwright/test";
+import { APIRequestContext, Browser, Page, TestInfo, expect, test } from "@playwright/test";
 import { CommentEntityType, UserGroup } from "common/enums";
 import { TestUser, disposeLogIn, logIn } from "../../utils/auth.js";
 import { randomString } from "../../utils/randomString.js";
+import { NewComment } from "common/interfaces";
 
 
-runTestSuite({ entityType: CommentEntityType.Event })
-runTestSuite({ entityType: CommentEntityType.Poll })
-runTestSuite({ entityType: CommentEntityType.SongLyric })
-runTestSuite({ entityType: CommentEntityType.WallPost })
+runTestSuite({ 
+    entityType: CommentEntityType.Event, 
+    entityId: 1
+})
+runTestSuite({ 
+    entityType: CommentEntityType.Poll,  
+    entityId: 1
+})
+runTestSuite({ 
+    entityType: CommentEntityType.SongLyric,  
+    entityId: 1
+})
+runTestSuite({ 
+    entityType: CommentEntityType.WallPost,  
+    entityId: 1
+})
 
 interface TestOptions {
-    entityType: CommentEntityType    
+    entityType: CommentEntityType,
+    entityId: number    
 }
 
 function runTestSuite(opts: TestOptions) {
     test.describe.serial(`Comment on ${testName(opts.entityType)}`, () => {  
 
         test.describe.serial("Author can update, edit and delete", () => { 
-            testAuthorCanUpdateEditAndDelete(opts.entityType)
+            testAuthorCanUpdateEditAndDelete(opts)
         })
     
-        testExternalDelete(opts.entityType)
+        test("Admin can delete", async ({browser}, workerInfo) => {
+            await runDeleteTestWithSetup({
+                entityType: opts.entityType, 
+                entityId: opts.entityId, 
+                deleter: UserGroup.Administrator,
+                browser: browser,
+                workerInfo: workerInfo
+            })
+        })
+    
+        test("Superadmin can delete", async ({browser}, workerInfo) => {
+            await runDeleteTestWithSetup({
+                entityType: opts.entityType, 
+                entityId: opts.entityId,
+                deleter: UserGroup.SuperAdministrator,
+                browser: browser,
+                workerInfo: workerInfo
+            })
+        })
     })
 }
 
-function testAuthorCanUpdateEditAndDelete( entityType: CommentEntityType ) { 
+function testAuthorCanUpdateEditAndDelete( {entityId, entityType}: TestOptions) { 
 
     let page: Page
     let user: TestUser
@@ -60,30 +92,69 @@ function testAuthorCanUpdateEditAndDelete( entityType: CommentEntityType ) {
     })
 
     test(`Delete `, async () => {    
-        // TODO    
-        test.skip()
+        await runDeleteTest({ entityType, entityId, comment, page })
     })
 }
 
-function testExternalDelete(entityType: CommentEntityType) { 
+async function runDeleteTest({
+    page, 
+    entityType, 
+    entityId, 
+    comment
+}: {
+    page: Page,
+    entityType: CommentEntityType,
+    entityId: number,
+    comment: string
+}) {
+    await clickMenuButton(page, comment)    // Assume we are already on the page
 
-    test("Admin can delete", async () => {
-        await testDelete(entityType, UserGroup.Administrator)
-    })
+    await Promise.all([
+        page.waitForResponse(buildApiUrl(entityType, entityId)),
+        clickDelete(page)
+    ])
 
-    test("Superadmin can delete", async () => {
-        await testDelete(entityType, UserGroup.SuperAdministrator)
-    })
+    const commentLocator = getCommentLocator(page, comment)
+    await expect(commentLocator).not.toBeVisible()
+}
 
-    async function testDelete(entityType: CommentEntityType, deleter: UserGroup) {
-        // TODO
-        test.skip()
+
+async function runDeleteTestWithSetup({
+    entityType, 
+    entityId, 
+    deleter, 
+    browser, 
+    workerInfo
+}: {
+    entityType: CommentEntityType,
+    entityId: number,
+    deleter: UserGroup,
+    browser: Browser,
+    workerInfo: TestInfo
+}) {
+    
+    const postCommentAs = async (group: UserGroup, comment: string) => {
+        const { page } = await logIn(browser, workerInfo, group)
+        await postCommentToApi(page.request, entityType, entityId, comment)
+        await disposeLogIn(page)
     }
+
+    // Create and post a new comment
+    const comment = randomString(`Comment on ${entityType} ${entityId}`)
+    await postCommentAs(UserGroup.Contributor, comment)
+
+    // Log in as the deleter
+    const { page } = await logIn(browser, workerInfo, deleter)
+    await gotoPage(page, entityType, entityId)
+    
+    // Run tests
+    await runDeleteTest({ entityType, entityId, comment, page })
+
+    await disposeLogIn(page)
 }
 
-async function gotoPage(page: Page, entityType: CommentEntityType) {
-    const id = 1
-    const url = buildUrl(entityType, id)
+async function gotoPage(page: Page, entityType: CommentEntityType, entityId: number = 1) {
+    const url = buildPageUrl(entityType, entityId)
     await page.goto(url)
 }
 
@@ -92,7 +163,23 @@ function getCommentLocator(page: Page, comment: string) {
         .filter({ hasText: comment })
 }
 
-function buildUrl(entityType: CommentEntityType, entityId: number): string {
+async function clickMenuButton(page: Page, comment: string) {
+    const button = getCommentLocator(page, comment)
+        .getByRole("button", {name: "Kommentarmeny"})
+    await button.click()
+} 
+
+async function clickDelete(page: Page) {
+    const button = page.getByRole("menuitem", {name: "Slett"})
+    await button.click()
+}
+
+async function clickEdit(page: Page) {
+    const button = page.getByRole("menuitem", {name: "Rediger"})
+    await button.click()
+}
+
+function buildPageUrl(entityType: CommentEntityType, entityId: number): string {
     switch(entityType) { 
         case CommentEntityType.Event:
             return `/arrangement/${entityId}`
@@ -104,6 +191,21 @@ function buildUrl(entityType: CommentEntityType, entityId: number): string {
             return `/vegg/${entityId}`
         default:
             throw "Not implemented"
+    }
+}
+
+function buildApiUrl(entityType: CommentEntityType, entityId: number): string { 
+    return `/api/${entityType}/${entityId}/comments`
+}
+
+// Posting a comment through the api is significantly faster than through the UI
+async function postCommentToApi(api: APIRequestContext, entityType: CommentEntityType, entityId: number, comment: string, ) { 
+    const data: NewComment = { 
+        comment: comment 
+    }
+    const res = await api.post(`/api/${entityType}/${entityId}/comments/new`, {data: data})
+    if(!res.ok()) {
+        throw new Error(`Failed to create ${entityType} comment.\n${await res.text()}`)
     }
 }
 
