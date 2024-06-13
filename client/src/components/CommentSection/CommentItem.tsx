08c7bf975839fd2dca437aeb96f4911a3338f01e
Torstein Wilhelmsen Sandvik <git@torsteinws.no>
2024-06-13 13:26:08 +0700

    [Refactor] Split up CommentItem into different components

--- client/src/components/CommentSection/CommentItem.tsx
+++ client/src/components/CommentSection/CommentItem.tsx
@@ -1,4 +1,5 @@
 import { Paper, Stack, useTheme } from "@mui/material";
+import { QueryKey } from "@tanstack/react-query";
 import { Comment } from "common/interfaces";
 import dayjs from "dayjs";
 import { LinkifiedText } from 'src/components/LinkifiedText';
@@ -10,13 +11,14 @@ import { UserFullName } from '../user/UserFullName';
 import { CommentSectionVariant } from "./types";
 
 export function CommentItem({
-    comment, style, variant,
+    comment, 
+    style, 
+    variant,
 }: {
-    comment: Comment;
-    style?: React.CSSProperties;
-    variant?: CommentSectionVariant;
+    comment: Comment,
+    style?: React.CSSProperties,
+    variant?: CommentSectionVariant,
 }) {
-    const theme = useTheme();
     return (
         <Stack
             direction="row"
@@ -29,79 +31,99 @@ export function CommentItem({
                 style={{
                     marginTop: "5px"
                 }} />
-            <div style={{
-                width: variant === "normal" ? "100%" : undefined,
-            }}>
-                <div
-                    style={{
-                        backgroundColor: theme.palette.divider,
-                        minWidth: "130px",
-                        padding: variant === "normal" ? "12px" : "7px 14px 10px 14px",
-                        borderRadius: variant === "normal" ? "10px" : "16px",
-                        position: "relative"
-                    }}
-                >
-                    <div>
-                        <UserFullName
-                            userId={comment.createdBy}
-                            style={{
-                                fontSize: variant === "normal" ? "inherit" : "small",
-                            }} />
-                    </div>
-                    <div
-                        style={{
-                            whiteSpace: "pre-line"
-                        }}>
-                        <LinkifiedText>
-                            {comment.comment}
-                        </LinkifiedText>
-                    </div>
-                    <div
-                        style={{
-                            position: "absolute",
-                            right: "0px",
-                            zIndex: 1
-                        }}
-                    >
-                        <Paper
-                            style={{
-                                borderRadius: "30px",
-                                lineHeight: "0px",
-                                padding: "0px",
-                                margin: "0px",
-                            }}
-                            elevation={4}
-                        >
-                            <LikeListIconButton
-                                maxItems={2}
-                                style={{
-                                    borderRadius: "30px",
-                                    fontSize: "13pt",
-                                    lineHeight: "15pt",
-                                    padding: "0px 2px 1px 2px",
-                                }} />
-                        </Paper>
-                    </div>
-                </div>
-                <div>
-                    <span>
-                        <LikeButton
-                            style={{
-                                fontSize: "small",
-                                marginInline: "4px",
-                                minWidth: "40px",
-                            }} />
-                    </span>
-                    <span
-                        style={{
-                            fontSize: "small",
-                            opacity: "0.6",
-                        }}
-                    >
-                        {dayjs.utc(comment.createdAt).locale(relativeTimeShortFormat).fromNow()}
-                    </span>
-                </div>
-            </div>
+            <ReadOnlyComment comment={comment} variant={variant}  />
         </Stack>
     );
 }
+
+function ReadOnlyComment({
+    variant,
+    comment,
+}: {
+    comment: Comment,
+    variant?: CommentSectionVariant,
+}) {
+    return (
+        <div style={{
+            width: variant === "normal" ? "100%" : undefined,
+        }}>
+            <CommentBubble comment={comment} variant={variant} />
+            <div>
+                <span>
+                    <LikeButton
+                        style={{
+                            fontSize: "small",
+                            marginInline: "4px",
+                            minWidth: "40px",
+                        }} />
+                </span>
+                <span
+                    style={{
+                        fontSize: "small",
+                        opacity: "0.6",
+                    }}
+                >
+                    {dayjs.utc(comment.createdAt).locale(relativeTimeShortFormat).fromNow()}
+                </span>
+            </div>
+        </div>
+    )
+}
+
+function CommentBubble({ comment, variant }: {comment: Comment, variant?: CommentSectionVariant}) {
+    const theme = useTheme();
+    return ( 
+        <div
+            style={{
+                backgroundColor: theme.palette.divider,
+                minWidth: "130px",
+                padding: variant === "normal" ? "12px" : "7px 14px 10px 14px",
+                borderRadius: variant === "normal" ? "10px" : "16px",
+                position: "relative"
+            }}
+        >
+            <div>
+                <UserFullName
+                    userId={comment.createdBy}
+                    style={{
+                        fontSize: variant === "normal" ? "inherit" : "small",
+                    }} />
+            </div>
+            <div
+                style={{
+                    whiteSpace: "pre-line"
+                }}>
+                <LinkifiedText>
+                    {comment.comment}
+                </LinkifiedText>
+            </div>
+            <div
+                style={{
+                    position: "absolute",
+                    right: "0px",
+                    zIndex: 1
+                }}
+            >
+                <Paper
+                    style={{
+                        borderRadius: "30px",
+                        lineHeight: "0px",
+                        padding: "0px",
+                        margin: "0px",
+                    }}
+                    elevation={4}
+                >
+                    <LikeListIconButton
+                        maxItems={2}
+                        style={{
+                            borderRadius: "30px",
+                            fontSize: "13pt",
+                            lineHeight: "15pt",
+                            padding: "0px 2px 1px 2px",
+                        }} />
+                </Paper>
+            </div>
+        </div>
+    )
+}
+
