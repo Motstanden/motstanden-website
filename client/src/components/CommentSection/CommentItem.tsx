fcf3e4c345292489fdda39c88860715fd3b55303
Torstein Wilhelmsen Sandvik <git@torsteinws.no>
2024-06-13 13:58:59 +0700

    Set up plumbing for mutating comments

--- client/src/components/CommentSection/CommentItem.tsx
+++ client/src/components/CommentSection/CommentItem.tsx
@@ -1,11 +1,17 @@
+import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
 import { Paper, Stack, useTheme } from "@mui/material";
-import { QueryKey } from "@tanstack/react-query";
+import { QueryKey, useMutation, useQueryClient } from "@tanstack/react-query";
 import { Comment } from "common/interfaces";
 import dayjs from "dayjs";
+import { useState } from "react";
 import { LinkifiedText } from 'src/components/LinkifiedText';
+import { useAuthenticatedUser } from "src/context/Authentication";
 import { relativeTimeShortFormat } from 'src/context/Locale';
 import { LikeButton } from '../likes/LikeButton';
 import { LikeListIconButton } from '../likes/LikeListButton';
+import { DeleteMenuItem } from "../menu/DeleteMenuItem";
+import { EditMenuItem } from "../menu/EditMenuItem";
+import { IconPopupMenu } from "../menu/IconPopupMenu";
 import { UserAvatar } from '../user/UserAvatar';
 import { UserFullName } from '../user/UserFullName';
 import { CommentSectionVariant } from "./types";
@@ -21,6 +27,53 @@ export function CommentItem({
     style?: React.CSSProperties,
     variant?: CommentSectionVariant,
 }) {
+    const queryClient = useQueryClient()
+
+    const editItem = useMutation({
+        mutationFn: async (comment: Comment) => {
+            await new Promise(resolve => setTimeout(resolve, 3000))
+        },
+        onError: () => {
+            window.alert("Fikk ikke til å redigere kommentaren.\nSi ifra til webansvarlig!")
+        },
+        onSuccess: async () => {
+            return await queryClient.invalidateQueries({ queryKey })
+        }
+    })
+
+    const deleteItem = useMutation({
+        mutationFn: async () => {
+            await new Promise(resolve => setTimeout(resolve, 3000))
+        },
+        onError: () => {
+            window.alert("Fikk ikke til å slette kommentaren.\nSi ifra til webansvarlig!")
+        },
+        onSuccess: async () => {
+            return await queryClient.invalidateQueries({ queryKey })
+        }
+    })
+
+    const [isEditing, setIsEditing] = useState(false)
+
+    const onEditClick = () => {
+        setIsEditing(true)
+    }
+
+    const onPostEdit = (comment: Comment) => {
+        editItem.mutate(comment)
+        setIsEditing(false)
+    }
+
+    const onAbortEditClick = () => {
+        setIsEditing(false)
+    }
+
+    const onDeleteClick = () => {
+        if(window.confirm("Vil du permanent slette denne kommentaren?")) {
+            deleteItem.mutate()
+        }
+    }
+
     return (
         <Stack
             direction="row"
@@ -33,7 +86,14 @@ export function CommentItem({
                 style={{
                     marginTop: "5px"
                 }} />
-            <ReadOnlyComment comment={comment} variant={variant}  />
+            {!isEditing && (
+                <ReadOnlyComment 
+                    comment={editItem.isPending ? editItem.variables : comment} 
+                    variant={variant}/>
+            )}
+            {isEditing && ( 
+                <EditCommentForm/>
+            )}
         </Stack>
     );
 }
@@ -49,7 +109,10 @@ function ReadOnlyComment({
         <div style={{
             width: variant === "normal" ? "100%" : undefined,
         }}>
-            <CommentBubble comment={comment} variant={variant} />
+            <Stack direction="row" alignItems="center" gap={{xs: 0.2, sm: 0.8}}>
+                <CommentBubble comment={comment} variant={variant} />
+                <CommentMenu comment={comment} />
+            </Stack>
             <div>
                 <span>
                     <LikeButton
@@ -129,3 +192,36 @@ function CommentBubble({ comment, variant }: {comment: Comment, variant?: Commen
     )
 }
 
+function EditCommentForm() {
+    return (
+        <>Todo: Create edit comment form...</>
+    )
+}
+
+function CommentMenu({
+    comment
+}: {
+    comment: Comment
+}) {
+
+    const {isAdmin, user} = useAuthenticatedUser()
+    const canEdit = user.id === comment.createdBy
+    const canDelete = canEdit || isAdmin 
+
+    if(!canEdit && !canDelete) 
+        return <></>
+
+    return (
+        <IconPopupMenu
+            icon={<MoreHorizIcon />}
+            ariaLabel="Kommentarmeny"
+        >
+            {canEdit && (
+                <EditMenuItem divider={canDelete}/>
+            )}
+            {canDelete && (
+                <DeleteMenuItem/>
+            )}
+        </IconPopupMenu>
+    )
+}
