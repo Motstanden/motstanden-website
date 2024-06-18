5d96f159e08406338e9f22de1aa09c931b8d84d5
Torstein Wilhelmsen Sandvik <git@torsteinws.no>
2024-06-19 03:08:40 +0700

    [Refactor] Remove unnecessary transaction

--- server/src/db/likes/upsert.ts
+++ server/src/db/likes/upsert.ts
@@ -6,19 +6,16 @@ import { likesTable } from "./tableNames.js"
 
 export function upsert(entityType: LikeEntityType, entityId: number, like: NewLike, userId: number): void {
     const db = new Database(motstandenDB, dbReadWriteConfig)
-    const startTransaction = db.transaction(() => {
-        const stmt = db.prepare(`
-            INSERT INTO 
-                ${likesTable.name(entityType)} (${likesTable.entityId(entityType)}, emoji_id, user_id) 
-            VALUES 
-                (?, ?, ?)
-            ON CONFLICT 
-                (${likesTable.entityId(entityType)}, user_id) 
-            DO UPDATE
-                SET emoji_id = EXCLUDED.emoji_id 
-        `)
-        stmt.run(entityId, like.emojiId, userId)
-    })
-    startTransaction()
+    const stmt = db.prepare(`
+        INSERT INTO 
+            ${likesTable.name(entityType)} (${likesTable.entityId(entityType)}, emoji_id, user_id) 
+        VALUES 
+            (?, ?, ?)
+        ON CONFLICT 
+            (${likesTable.entityId(entityType)}, user_id) 
+        DO UPDATE
+            SET emoji_id = EXCLUDED.emoji_id 
+    `)
+    stmt.run(entityId, like.emojiId, userId)
     db.close()
 }
