993e6e713549855de10a1b86565f4d49d09070b1
Torstein Wilhelmsen Sandvik <git@torsteinws.no>
2024-06-21 06:09:58 +0700

    Add api function for creating new song lyrics

--- tests/utils/api/index.ts
+++ tests/utils/api/index.ts
@@ -1,7 +1,11 @@
 import { commentsApi } from "./comments.js"
+import { songLyricsApi } from "./songLyric.js"
 
 export const api = {
     comments: {
         ...commentsApi
+    },
+    songLyrics: {
+        ...songLyricsApi
     }
 }
---
+++ tests/utils/api/songLyric.ts
@@ -0,0 +1,13 @@
+import { APIRequestContext } from "@playwright/test"
+import { NewSongLyric } from "common/interfaces"
+
+async function createSongLyric(api: APIRequestContext, lyric: NewSongLyric) {
+    const res = await api.post("/api/song-lyric/new", { data: lyric })
+    if (!res.ok()) {
+        throw new Error(`Failed to create song lyric.\n${await res.text()}`)
+    }
+}
+
+export const songLyricsApi = {
+    new: createSongLyric
+}
