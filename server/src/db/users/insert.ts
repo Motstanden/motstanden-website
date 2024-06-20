7338ea44c97ab57ba328895651142db2f3c4325d
Torstein Wilhelmsen Sandvik <git@torsteinws.no>
2024-06-20 20:39:05 +0700

    [Refactor] Clean up code

--- server/src/db/users/insert.ts
+++ server/src/db/users/insert.ts
@@ -1,70 +1,57 @@
 import Database from "better-sqlite3"
 import { UserGroup, UserRank, UserStatus } from "common/enums"
 import { NewUser } from "common/interfaces"
-import { dbReadOnlyConfig, dbReadWriteConfig, motstandenDB } from "../../config/databaseConfig.js"
+import { dbReadWriteConfig, motstandenDB } from "../../config/databaseConfig.js"
 import { userGroupsDb } from "./groups/index.js"
 import { userRanksDb } from "./ranks/index.js"
 import { userStatusDb } from "./status/index.js"
 
 
 export function insertUser(user: NewUser): number | bigint {
-    const dbRd = new Database(motstandenDB, dbReadOnlyConfig) // Read only instance of db
+    const db = new Database(motstandenDB, dbReadWriteConfig) // Read only instance of db
 
+    // Default values
+    const groupId = userGroupsDb.getId(UserGroup.Contributor, db)
+    const rankId = userRanksDb.getId(UserRank.ShortCircuit, db)
+    const statusId = userStatusDb.getId(UserStatus.Active, db)
 
-    // Throws exceptions if not found
-    const groupId = userGroupsDb.getId(UserGroup.Contributor, dbRd)
-    const rankId = userRanksDb.getId(UserRank.ShortCircuit, dbRd)
-    const statusId = userStatusDb.getId(UserStatus.Active, dbRd)
-    dbRd.close()
-
-    const dbWr = new Database(motstandenDB, dbReadWriteConfig) // Read/Write instance of db
-
-
-    // Define transaction
-    let result: Database.RunResult | undefined
-    const startTransaction = dbWr.transaction(() => {
-        const stmt = dbWr.prepare(`
-            INSERT INTO user(
-                user_group_id, 
-                user_rank_id, 
-                email, 
-                first_name, 
-                middle_name, 
-                last_name, 
-                profile_picture,
-                cape_name,
-                phone_number,
-                birth_date,
-                user_status_id,
-                end_date
-            )
-            VALUES
-                (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
-        `)
-        result = stmt.run(
-            groupId,
-            rankId,
-            user.email,
-            user.firstName,
-            user.middleName,
-            user.lastName,
-            user.profilePicture,
-            "", // cape name
-            null, // Phone number
-            null, // Birth date
-            statusId,
-            null // End date
+    const stmt = db.prepare(`
+        INSERT INTO user(
+            first_name, 
+            middle_name, 
+            last_name, 
+            email, 
+            profile_picture,
+            user_group_id, 
+            user_rank_id, 
+            user_status_id
         )
+        VALUES ( 
+            @firstName, 
+            @middleName, 
+            @lastName, 
+            @email, 
+            @profilePicture, 
+            @groupId, 
+            @rankId, 
+            @statusId 
+        )
+    `)
+    const result = stmt.run({
+        firstName: user.firstName,
+        middleName: user.middleName,
+        lastName: user.lastName,
+        email: user.email,
+        profilePicture: user.profilePicture,
+        groupId: groupId,
+        rankId: rankId,
+        statusId: statusId,
     })
 
-    // Run transaction
-    startTransaction()
-    dbWr.close()
+    db.close()
 
-    if (result && result.changes > 0)
+    if (result.changes > 0)
         return result.lastInsertRowid
-
     else
-        throw "something went wrong"
-
+        throw "Failed to update user"
 }
