65e6677e6b8ec314dadab9eaaaa4aa16b99fbd40
Torstein Wilhelmsen Sandvik <git@torsteinws.no>
2024-06-20 21:48:00 +0700

    Expose user-groups/ranks/status db functions

--- server/src/db/users/index.ts
+++ server/src/db/users/index.ts
@@ -1,6 +1,9 @@
 import { getAllAsUserReference, getAllUsers, getUser, getUserByMail, userExists } from "./get.js"
+import { userGroupsDb } from "./groups/index.js"
 import { insertUser } from "./insert.js"
+import { userRanksDb } from "./ranks/index.js"
 import { refreshTokensDb } from "./refreshTokens/index.js"
+import { userStatusDb } from "./status/index.js"
 import { updateUser } from "./update.js"
 
 export const usersDb = {
@@ -13,7 +16,17 @@ export const usersDb = {
     insert: insertUser,
     update: updateUser,
 
+    groups: {
+        ...userGroupsDb
+    },
+    ranks: {
+        ...userRanksDb
+    },
+    status: {
+        ...userStatusDb
+    },
+    
     refreshTokens: {
         ...refreshTokensDb
-    }
+    },
 }
