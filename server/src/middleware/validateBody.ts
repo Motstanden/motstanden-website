85820014c647e10568becc770ef66e8f7e51babd
Torstein Wilhelmsen Sandvik <git@torsteinws.no>
2024-06-22 05:09:38 +0700

    Overwrite req.body with parsed value

--- server/src/middleware/validateBody.ts
+++ server/src/middleware/validateBody.ts
@@ -1,13 +1,14 @@
 import { NextFunction, Request, Response } from 'express'
 import { z } from 'zod'
 
-export function validateBody(schema: z.ZodSchema) {
+export function validateBody<T>(schema: z.ZodSchema<T>) {
     return (req: Request, res: Response, next: NextFunction) => {
         const result = schema.safeParse(req.body);
         if(result.success) { 
+            req.body = result.data
             next()
         } else {
             res.status(400).json(result.error.format());
         }
-    };
+    }
 }
