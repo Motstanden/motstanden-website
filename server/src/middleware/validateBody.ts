1a94665c7af44b7283c822ef513c53abd781b745
Torstein Wilhelmsen Sandvik <git@torsteinws.no>
2024-06-23 21:28:38 +0700

    Send prettier error messages on validation fails

--- server/src/middleware/validateBody.ts
+++ server/src/middleware/validateBody.ts
@@ -1,5 +1,6 @@
 import { NextFunction, Request, Response } from 'express'
 import { z } from 'zod'
+import { fromError } from 'zod-validation-error'
 
 export function validateBody<T>(schema: z.ZodSchema<T>) {
     return (req: Request, res: Response, next: NextFunction) => {
@@ -8,7 +9,10 @@ export function validateBody<T>(schema: z.ZodSchema<T>) {
             req.body = result.data
             next()
         } else {
-            res.status(400).json(result.error.format());
+            res.status(400).json({
+                message: "Invalid request body",
+                error: fromError(result.error).toString()
+            });
         }
     }
 }
