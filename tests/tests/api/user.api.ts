84e508e7669d7605d9295ed72631646ba6a030bd
Torstein Wilhelmsen Sandvik <git@torsteinws.no>
2024-06-27 03:31:38 +0700

    [WIP] Write more tests for updating users

--- tests/tests/api/user.api.ts
+++ tests/tests/api/user.api.ts
@@ -1,10 +1,10 @@
-import { APIRequestContext, expect, test } from '@playwright/test'
+import { APIRequestContext, APIResponse, expect, test } from '@playwright/test'
 import { UserGroup, UserRank, UserStatus } from 'common/enums'
-import { NewUser, User } from 'common/interfaces'
+import { NewUser, User, UserUpdateAsSelf, UserUpdateMembership, UserUpdateRole } from 'common/interfaces'
 import { randomUUID } from 'crypto'
 import { z } from "zod"
 import dayjs from "../../lib/dayjs.js"
-import { apiLogIn } from '../../utils/auth.js'
+import { apiLogIn, unsafeApiLogIn } from '../../utils/auth.js'
 import { dateTimeSchema } from '../../utils/zodtypes.js'
 
 const userSchema = z.object({ 
@@ -86,8 +86,8 @@ test.describe.serial("Create and update user", () => {
 
         const uuid: string = randomUUID().toLowerCase()
         const newUser: NewUser = {
-            firstName: "__Test",
-            middleName: "User",
+            firstName: "__Test_1",
+            middleName: "User_1",
             lastName: uuid,
             email: `${uuid}@motstanden.no`,
             profilePicture: "files/private/profilbilder/girl.png"
@@ -118,30 +118,59 @@ test.describe.serial("Create and update user", () => {
         user = actualUser
     })
 
-    test("PATCH /api/users/me", async ({request}, workerInfo) => { 
-        throw "Not implemented"
+    test("PATCH /api/users/me", async ({request}) => { 
+        await unsafeApiLogIn(request, user.email)
+
+        const uuid: string = randomUUID().toLowerCase()
+        const updatedData: UserUpdateAsSelf = {
+            firstName: "___Test_2",
+            middleName: "___User_2",
+            lastName: uuid,
+            email: `${uuid}@motstanden.no`,
+            phoneNumber: 12345678,
+            birthDate: dayjs().subtract(20, "years").utc().format("YYYY-MM-DD"),
+            startDate: dayjs().subtract(3, "years").utc().format("YYYY-MM-DD"),
+            endDate: dayjs().subtract(1, "years").utc().format("YYYY-MM-DD"),
+        }
+
+        await updateUser(request, { type: "self", data: updatedData })
+        const actualUser = await getUser(request, user.id)
+        const expectedUser: User = {
+            ...user,
+            ...updatedData,
+            updatedAt: actualUser.updatedAt,
+            createdAt: actualUser.createdAt,
+        }
+
+        assertEqualUsers(actualUser, expectedUser)
+        user = actualUser
     })
 
     test.describe.serial("PUT /api/users/:id/role", () => { 
 
         test("Admin can promote to admin", async ({request}, workerInfo) => { 
-            throw "Not implemented"
+            await apiLogIn(request, workerInfo, UserGroup.Administrator)
+            user = await testUpdateRole(request, user, UserGroup.Administrator)
         })
 
         test("Admin can not promote to super admin", async ({request}, workerInfo) => { 
-            throw "Not implemented"
+            await apiLogIn(request, workerInfo, UserGroup.Administrator)
+            await testForbiddenUpdateRole(request, user, UserGroup.SuperAdministrator)
         })
 
         test("Super admin can promote to super admin", async ({request}, workerInfo) => { 
-            throw "Not implemented"
+            await apiLogIn(request, workerInfo, UserGroup.SuperAdministrator)
+            user = await testUpdateRole(request, user, UserGroup.SuperAdministrator)
         })
 
         test("Admin can not demote super admin", async ({request}, workerInfo) => { 
-            throw "Not implemented"
+            await apiLogIn(request, workerInfo, UserGroup.Administrator)
+            await testForbiddenUpdateRole(request, user, UserGroup.Contributor)
         })
 
         test("Super admin can demote super admin", async ({request}, workerInfo) => { 
-            throw "Not implemented"
+            await apiLogIn(request, workerInfo, UserGroup.SuperAdministrator)
+            user = await testUpdateRole(request, user, UserGroup.Contributor)
         })
     })
     
@@ -153,7 +182,6 @@ test.describe.serial("Create and update user", () => {
         throw "Not implemented"
     })
 })
-
 
 
 function assertEqualUsers(user1: User, user2: User) { 
@@ -174,6 +202,37 @@ function assertEqualUsers(user1: User, user2: User) {
     expect(user1.endDate).toBe(user2.endDate)
 
     // No need to compare createdAt and updatedAt
+}
+
+interface TestPromoteArgs {
+    request: APIRequestContext
+    user: User
+    newGroup: UserGroup
+}
+
+async function testUpdateRole(request: APIRequestContext, user: User, newGroup: UserGroup ): Promise<User> { 
+    const res = await updateRole(request, user, newGroup)
+    expect(res.ok(), `Failed to update role\n${res.status()}: ${res.statusText()}`).toBeTruthy()
+    
+    const updatedUser = await getUser(request, user.id)
+    expect(updatedUser.groupName).toBe(newGroup)
+    
+    return updatedUser
+}
+
+async function testForbiddenUpdateRole(request: APIRequestContext, user: User, newGroup: UserGroup ) { 
+    const res = await updateRole(request, user, newGroup)
+    expect(res.status(), `Expected 403 forbidden but got ${res.status()} ${res.statusText()}`).toBe(403)
+    const updatedUser = await getUser(request, user.id)
+    expect(updatedUser.groupName).not.toBe(newGroup)
+}
+
+async function updateRole(request: APIRequestContext, user: User, newGroup: UserGroup) { 
+    const body: UserUpdateRole = {
+        groupName: newGroup
+    }
+    const res = await request.put(`/api/users/${user.id}/role`, { data: body })
+    return res
 }
 
 async function createUser(request: APIRequestContext, newUser: NewUser) {
@@ -199,4 +258,33 @@ async function getUser(request: APIRequestContext, id: number) {
     }
     const user = await res.json() as User
     return user
+}
+
+type UpdateType = {
+    type: "self",
+    data: UserUpdateAsSelf
+} | {
+    type: "membership",
+    data: UserUpdateMembership
+} | {
+    type: "superadmin",
+    data: UserUpdateMembership
+}
+
+async function updateUser(request: APIRequestContext,  {type, data} : UpdateType)  {
+    let res: APIResponse 
+    switch(type) { 
+        case "self":
+            res = await request.patch("/api/users/me", { data: data })
+            break
+        case "membership":
+            res = await request.put("/api/users/:id/membership", { data: data })
+            break
+        case "superadmin":
+            res = await request.patch("/api/users/:id/membership", { data: data })
+            break
+    }
+    if(!res.ok()) {
+        throw new Error(`Failed to update user.\n${res.status()}: ${await res.text()}`)
+    }
 }
