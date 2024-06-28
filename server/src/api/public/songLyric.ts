1de285f951c334bcc1fac0b5e44fd7bd5cbb3ee6
Torstein Wilhelmsen Sandvik <git@torsteinws.no>
2024-06-29 03:24:37 +0700

    Refactor song lyrics api

--- server/src/api/public/songLyric.ts
+++ server/src/api/public/songLyric.ts
@@ -2,58 +2,48 @@ import { strToNumber } from "common/utils"
 import express, { Request, Response } from "express"
 import { db } from "../../db/index.js"
 import { AuthenticateUser } from "../../middleware/jwtAuthenticate.js"
-import { validateNumber } from "../../middleware/validateNumber.js"
+import { validateParams } from "../../middleware/zodValidation.js"
+import { Schemas } from "../../utils/zodSchema.js"
 
 const router = express.Router()
 
 router.get("/song-lyric/simple-list", (req, res) => { 
     const lyrics = db.songLyrics.getSimplifiedList()
-    res.send(lyrics)
+    res.json(lyrics)
 })
 
 router.get("/private/song-lyric/:id", 
     AuthenticateUser(),
-    validateNumber({
-        getValue: (req) => req.params.id,
-        failureMessage: "Could not parse song lyric id"
-    }),
-    sendSongLyric({ 
-        isPublic: false 
-    })
+    validateParams(Schemas.params.id),
+    (req, res) => {
+        const { id } = Schemas.params.id.parse(req.params)
+        const lyric = db.songLyrics.get(id)
+
+        if(lyric === undefined)
+            return res.status(404).send("Song lyric not found")
+
+        res.json(lyric)
+    }
 )
 
 router.get("/public/song-lyric/:id",
-    validateNumber({
-        getValue: (req) => req.params.id,
-        failureMessage: "Could not parse song lyric id"
-    }),
-    sendSongLyric({ 
-        isPublic: true 
-    })
-)
+    validateParams(Schemas.params.id),
+    (req, res) => {
+        const { id } = Schemas.params.id.parse(req.params)
 
-function sendSongLyric({ isPublic }: {isPublic: boolean} ) {
-    return (req: Request, res: Response) => {
-        const id = strToNumber(req.params.id) as number
-        try {
-            const privateLyric = db.songLyrics.get(id)
+        const privateLyric = db.songLyrics.get(id)
 
-            if(!privateLyric)
-                return res.status(404).send("Song lyric not found");
-            
-            const filteredLyric = isPublic ? {
-                id: privateLyric.id,
-                title: privateLyric.title,
-                content: privateLyric.content
-            } : privateLyric
-            
-            res.send(filteredLyric)
-        } catch (err) {
-            console.error(err)
-            res.status(500).end()
+        if(privateLyric === undefined)
+            return res.status(404).send("Song lyric not found")
+
+        const filteredLyric = {
+            id: privateLyric.id,
+            title: privateLyric.title,
+            content: privateLyric.content
         }
+        res.json(filteredLyric)
     }
-}
+)
 
 export {
     router as publicSongLyricApi
