e5c4ecefdcc09fdd237780f7b4997fefd24d2e86
Torstein Wilhelmsen Sandvik <git@torsteinws.no>
2024-06-30 00:52:51 +0700

    Add conditional edit button to card component

--- client/src/routes/user/components/Card.tsx
+++ client/src/routes/user/components/Card.tsx
@@ -1,19 +1,42 @@
-import { Grid, Stack } from "@mui/material";
-import { TitleCard } from "src/components/TitleCard";
+import EditIcon from "@mui/icons-material/Edit"
+import { Grid, IconButton, Stack, SxProps, Tooltip } from "@mui/material"
+import { TitleCard } from "src/components/TitleCard"
 
 export function Card({
     title,
     children,
-    spacing
+    spacing,
+    showEditButton,
+    onEditClick,
+    editButtonToolTip
 }:{
     title: string,
     spacing?: number
-    children: React.ReactNode
+    children: React.ReactNode,
+    showEditButton?: boolean
+    onEditClick?: () => void
+    editButtonToolTip?: string
 }) {
     return (
-
         <Grid item xs={12} md={6}>
-            <TitleCard title={title} sx={{ height: "100%" }}>
+            <TitleCard 
+                title={title} 
+                showMenu={showEditButton}
+                noMenuMargin
+                menu={(
+                    <EditButton 
+                        onClick={onEditClick} 
+                        toolTip={editButtonToolTip} 
+                        sx={{
+                            marginRight: "-9px",
+                            marginTop: "2px"
+                        }}
+                        />
+                )}
+                sx={{ 
+                    height: "100%" 
+                }}
+                >
                 <Stack spacing={spacing ?? 2}>
                     {children}
                 </Stack>
@@ -48,5 +71,27 @@ export function CardTextItem({ label, text }: { label: string, text: string }) {
                 </span>
             </div>
         </>
+    )
+}
+
+function EditButton({ 
+    toolTip, onClick, sx
+}: { 
+    toolTip?: string, 
+    onClick?: () => void,
+    sx?: SxProps
+}) {
+
+    return (
+        <Tooltip title={toolTip}>
+            <IconButton 
+                onClick={onClick} 
+                sx={{
+                    p: 0.8,
+                    ...sx
+                }}>
+                <EditIcon fontSize="small" />
+            </IconButton>
+        </Tooltip>
     )
 }
