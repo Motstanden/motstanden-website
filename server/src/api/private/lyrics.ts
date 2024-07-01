b3e42788d1f6c77f38c1aa5e774ab4830eb01ed9
Torstein Wilhelmsen Sandvik <git@torsteinws.no>
2024-07-01 19:17:08 +0700

    Add validation and thereby simplify api handlers

--- server/src/api/private/lyrics.ts
+++ server/src/api/private/lyrics.ts
@@ -1,14 +1,12 @@
-// TODO: Split up this file and put it into private/public folders
-
 import { UserGroup } from "common/enums"
 import { strToNumber } from "common/utils"
 import express from "express"
 import { z } from "zod"
 import { db } from "../../db/index.js"
 import { requiresGroupOrAuthor } from "../../middleware/requiresGroupOrAuthor.js"
-import { validateNumber } from "../../middleware/validateNumber.js"
-import { validateBody } from "../../middleware/zodValidation.js"
+import { validateBody, validateParams } from "../../middleware/zodValidation.js"
 import { getUser } from "../../utils/getUser.js"
+import { Schemas } from "../../utils/zodSchema.js"
 
 const router = express.Router()
 
@@ -18,62 +16,41 @@ const NewSongLyricSchema = z.object({
     isPopular: z.boolean()
 })
 
-router.post("/lyrics/:id/update",
-    validateNumber({
-        getValue: (req) => req.params.id,
-    }),
-    validateBody(NewSongLyricSchema),
-    (req, res) => {
-
-        // Validated by middleware
-        const id = strToNumber(req.params.id) as number
-        const user = getUser(req)
-        const newLyric = NewSongLyricSchema.parse(req.body)
-
-        if(!newLyric)
-            return res.status(400).send("Could not parse song lyric data")
-
-        try {
-            db.songLyrics.update(newLyric, id, user.userId)
-        } catch (err) {
-            console.log(err)
-            res.status(500).send("Failed to update song lyric in the database")
-        }
-        res.end()
-    }
-)
 
 router.post("/lyrics/new",
     validateBody(NewSongLyricSchema),
     (req, res) => {
-
-        // Validated by middleware
         const user = getUser(req)
         const newLyric = NewSongLyricSchema.parse(req.body)
 
-        try {
-            db.songLyrics.insert(newLyric, user.userId)
-        } catch (err) {
-            console.log(err)
-            res.status(400).send("Failed to insert new song lyric into the database")
-        }
+        db.songLyrics.insert(newLyric, user.userId)
+        res.end()
+    }
+)
+
+router.post("/lyrics/:id/update",
+    validateParams(Schemas.params.id),
+    validateBody(NewSongLyricSchema),
+    (req, res) => {
+        const user = getUser(req)
+        const { id } = Schemas.params.id.parse(req.params)
+        const newLyric = NewSongLyricSchema.parse(req.body)
+
+        db.songLyrics.update(newLyric, id, user.userId)
         res.end()
     }
 )
 
 router.post("/lyrics/:id/delete",
+    validateParams(Schemas.params.id),
     requiresGroupOrAuthor({
         requiredGroup: UserGroup.Administrator,
         getId: (req) => strToNumber(req.params.id),
         getAuthorInfo: (id) => db.songLyrics.get(id)
     }),
     (req, res) => {
-        const id = strToNumber(req.params.id) as number     // is validated by middleware
-        try {
-            db.songLyrics.delete(id)
-        } catch (err) {
-            res.status(500).send("Failed to delete song lyric from the database")
-        }
+        const { id } = Schemas.params.id.parse(req.params)
+        db.songLyrics.delete(id)
         res.end()
     }
 )
