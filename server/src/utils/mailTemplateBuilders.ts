e66a736861ab2dc25231bc89ba90df47f23ea27d
Torstein Wilhelmsen Sandvik <git@torsteinws.no>
2024-08-27 17:32:05 +0700

    Refactor: Make functions reusable

--- server/src/utils/mailTemplateBuilders.ts
+++ server/src/utils/mailTemplateBuilders.ts
@@ -1,8 +1,21 @@
 import fs from "fs/promises"
 
+const basePath = "../../assets/mail-templates" 
+
+enum Templates {
+    MagicLink = `${basePath}/MagicLink.html`,
+    DeletedUser = `${basePath}/DeletedUser.html`,
+    RestoredDeletedUser = `${basePath}/RestoredDeletedUser.html`,
+    Welcome = `${basePath}/Welcome.html`,
+}
+
+async function readFile(filePath: Templates) {
+    const url = new URL(filePath.valueOf(), import.meta.url)  
+    return await fs.readFile(url, "utf-8")
+}
+
 async function buildMagicLinkHtml(url: string, code: string): Promise<string> {
-    const filePath = new URL(`../../assets/mail-templates/MagicLink.html`, import.meta.url)
-    const html = await fs.readFile(filePath, "utf-8")
+    const html = await readFile(Templates.MagicLink)
 
     const date = new Date().toLocaleString("no-no", { timeZone: "cet" })
 
