1b1df3717724d4ced8ff47f9b3b1b29917af0069
Torstein Wilhelmsen Sandvik <git@torsteinws.no>
2024-08-27 17:55:27 +0700

    Make MagicLink mail prettier

--- server/src/utils/mailTemplateBuilders.ts
+++ server/src/utils/mailTemplateBuilders.ts
@@ -9,19 +9,15 @@ enum Templates {
     Welcome = `${basePath}/Welcome.html`,
 }
 
-async function readFile(filePath: Templates) {
-    const url = new URL(filePath.valueOf(), import.meta.url)  
-    return await fs.readFile(url, "utf-8")
-}
-
 async function buildMagicLinkHtml(url: string, code: string): Promise<string> {
     const html = await readFile(Templates.MagicLink)
 
-    const date = new Date().toLocaleString("no-no", { timeZone: "cet" })
+    const {date, time} = getMagicLinkExpireTime()
 
-    return html.replace("${magiclink}", url)
-        .replace("${verificationcode}", code)
-        .replace("${timestamp}", `${date}`)
+    return html.replace("${magicLink}", url)
+        .replace("${verificationCode}", code)
+        .replace("${linkExpireDate}", `${date}`)
+        .replace("${linkExpireTime}", `${time}`)
 }
 
 async function buildDeletedUserHtml(): Promise<string> {
@@ -35,6 +31,25 @@ async function buildRestoredDeletedUserHtml(): Promise<string> {
 async function buildWelcomeHtml(): Promise<string> {
     throw new Error("Not implemented")
 }
+
+async function readFile(filePath: Templates) {
+    const url = new URL(filePath.valueOf(), import.meta.url)  
+    return await fs.readFile(url, "utf-8")
+}
+
+// The magic link is valid for 1 hour from now
+function getMagicLinkExpireTime(): { date: string, time: string } {
+    const date = new Date()
+    date.setHours(date.getHours() + 1)
+
+    const datePart = date.toLocaleDateString("no-no", { timeZone: "cet" })
+    const timePart = date.toLocaleTimeString("no-no", { timeZone: "cet", hour: '2-digit', minute: '2-digit' })
+
+    return {
+        date: datePart,
+        time: timePart
+    }
+} 
 
 export const mailTemplates = {
     buildMagicLinkHtml,
