b42b24de7fdf583a97e0f84eee615dd999dc6a85
Torstein Wilhelmsen Sandvik <git@torsteinws.no>
2024-09-04 00:27:59 +0700

    Test that alle likes are deleted after a user has been deleted

--- tests/tests/jobs/deleteDeactivatedUsers.spec.ts
+++ tests/tests/jobs/deleteDeactivatedUsers.spec.ts
@@ -1,20 +1,32 @@
 import { APIRequestContext, expect, test } from "@playwright/test"
-import { UserGroup } from "common/enums"
-import { DeactivatedUser, User } from "common/interfaces"
+import { LikeEntityType, UserGroup } from "common/enums"
+import { DeactivatedUser, Like, User } from "common/interfaces"
 import dayjs from "dayjs"
 import sinon from "sinon"
 import { job } from "../../../server/src/jobs/deleteDeactivatedUsers.js"
 import { api } from "../../utils/api/index.js"
-import { apiLogIn } from "../../utils/auth.js"
+import { apiLogIn, unsafeApiLogIn } from "../../utils/auth.js"
 import { loadServerEnv } from "../../utils/loadServerEnv.js"
 
-test("Job deletes all identifiable user data", async ({ request, page }, workerInfo) => {
-    await apiLogIn(request, workerInfo, UserGroup.SuperAdministrator)
-    
+test("Job deletes all identifiable user data", async ({ request }, workerInfo) => {
+
+    // Create user and log in as user
     const user = await api.users.createRandom(workerInfo)
+    await unsafeApiLogIn(request, user.email)
+    
+    // Like all entities
+    const likeEntityId = 1
+    await api.likes.create(request, LikeEntityType.EventComment, likeEntityId)
+    await api.likes.create(request, LikeEntityType.PollComment, likeEntityId)
+    await api.likes.create(request, LikeEntityType.SongLyricComment, likeEntityId)
+    await api.likes.create(request, LikeEntityType.WallPost, likeEntityId)
+    await api.likes.create(request, LikeEntityType.WallPostComment, likeEntityId)
+
+
+    // Deactivate user
     await api.users.delete(workerInfo, user.id)
 
-    // Fast forward 90 days
+    // Fast forward the clock 90 days
     const clock = sinon.useFakeTimers({
         now: dayjs().add(90, "days").toDate(),
         toFake: ["Date"]
@@ -23,26 +35,35 @@ test("Job deletes all identifiable user data", async ({ request, page }, workerI
     loadServerEnv()
     await job()
 
+    // Restore the clock
     clock.restore()
 
-    // User is deleted if it is not found in the list of all active users nor the list of all deactivated users
-    expect(await isActivated(request, user.id)).toBe(false)
-    expect(await isDeactivated(request, user.id)).toBe(false)
+    // Log in as super admin
+    await apiLogIn(request, workerInfo, UserGroup.SuperAdministrator)
+
+    // Assert user is deleted
+    expect(await getUser(request, user.id)).toBe(undefined)
+    expect(await getDeactivatedUser(request, user.id)).toBe(undefined)
+
+    // Assert all likes are deleted
+    expect(await getLike(request, user.id, LikeEntityType.EventComment, likeEntityId)).toBe(undefined)
+    expect(await getLike(request, user.id, LikeEntityType.PollComment, likeEntityId)).toBe(undefined)
+    expect(await getLike(request, user.id, LikeEntityType.SongLyricComment, likeEntityId)).toBe(undefined)
+    expect(await getLike(request, user.id, LikeEntityType.WallPost, likeEntityId)).toBe(undefined)
+    expect(await getLike(request, user.id, LikeEntityType.WallPostComment, likeEntityId)).toBe(undefined)
 })
 
-async function isDeactivated(request: APIRequestContext, userId: number): Promise<boolean> {
-    const res = await request.get("/api/users/deactivated")
-    const allDeactivatedUsers: DeactivatedUser[] = await res.json()
-    const user = allDeactivatedUsers.find(u => u.id === userId)
-    return user !== undefined
+async function getUser(request: APIRequestContext, userId: number) : Promise<User | undefined> {
+    const users = await api.users.getAll(request)
+    return users.find(u => u.id === userId)
 }
 
-async function isActivated(request: APIRequestContext, userId: number) {
-    const res = await request.get("/api/users")
-    if(res.status() === 404)
-        return false
+async function getDeactivatedUser(request: APIRequestContext, userId: number): Promise<DeactivatedUser | undefined> {
+    const users = await api.users.getAllDeactivated(request)
+    return users.find(u => u.id === userId)
+}
 
-    const allUsers: User[] = await res.json()
-    const user = allUsers.find(u => u.id === userId)
-    return user !== undefined
+async function getLike(request: APIRequestContext, userId: number, entityType: LikeEntityType, entityId: number): Promise<Like | undefined> {
+    const allLikes = await api.likes.getAll(request, entityType, entityId)
+    return allLikes.find(l => l.userId === userId)
 }
