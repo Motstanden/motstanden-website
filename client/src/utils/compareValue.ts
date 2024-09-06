4d32b98104dbf07622549e9fe21806a99d0ac592
Torstein Wilhelmsen Sandvik <git@torsteinws.no>
2024-09-06 22:54:40 +0700

    Assign the highest value to empty strings

--- client/src/utils/compareValue.ts
+++ client/src/utils/compareValue.ts
@@ -9,13 +9,19 @@ function compareByBoolean(a: boolean, b: boolean, sortDirection: "asc" | "desc")
 }
 
 function compareByTimestamp(a: Dayjs | undefined, b: Dayjs | undefined, sortDirection: "asc" | "desc"): number {
-    const aValue = a?.unix() ?? 0;
-    const bValue = b?.unix() ?? 0;
+    const aValue = a?.unix() ?? Number.MAX_SAFE_INTEGER;
+    const bValue = b?.unix() ?? Number.MAX_SAFE_INTEGER;
     return sortDirection === "asc" ? aValue - bValue : bValue - aValue;
 }
 
 function compareByAlphanumerical(a: string, b: string, sortDirection: "asc" | "desc"): number {
     const collator = new Intl.Collator(undefined, { numeric: true, sensitivity: "base" });
+
+    // Put empty strings last when sorting in ascending order
+    if (a === "" && b === "") return 0;
+    if (a === "") return sortDirection === "asc" ? 1 : -1;
+    if (b === "") return sortDirection === "asc" ? -1 : 1;
+
     return sortDirection === "asc" ? collator.compare(a, b) : collator.compare(b, a);
 }
 
