e3e78fad9900c67a18db6a5d001272d0a5ee10bd
Torstein Wilhelmsen Sandvik <git@torsteinws.no>
2024-10-10 14:43:22 +0200

    WIP: Apply some styling to quotes and rumours

--- client/src/routes/home/Feed/FeedItems/Quote.tsx
+++ client/src/routes/home/Feed/FeedItems/Quote.tsx
@@ -1,7 +1,9 @@
-import { Box } from "@mui/material"
+import { Box, Link } from "@mui/material"
 import { Quote } from "common/interfaces"
 import { QuoteFeedItem as QuoteFeedItemType } from "common/types"
 import { QuoteList } from "src/routes/quotes/ListPage"
+import { Link as RouterLink } from "react-router-dom"
+import QuotesIcon from '@mui/icons-material/FormatQuote'
 
 export function QuoteFeedItem({ 
     data, 
@@ -18,16 +20,38 @@ export function QuoteFeedItem({
     } satisfies Quote))
     
     return (
-        <Box sx={{
-            borderLeftStyle: "solid",
-            borderLeftWidth: "6px",
-            borderLeftColor: theme => theme.palette.divider,
-            paddingLeft: "15px",
-        }}>
-            <QuoteList 
-                quotes={quotes}
-                onItemChanged={onItemChanged}
-            />
-        </Box>
+        <div>
+            <QuotesIcon sx={{
+                marginRight: "9px",
+                marginLeft: "-8px",
+                marginBottom: "-7px",
+                opacity: 0.8,
+            }}/>
+            <Link
+                color="Inherit"
+                component={RouterLink}
+                to={`/sitater`}
+                underline="hover"
+                sx={{
+                    fontWeight: "bold",
+                    fontSize: "small",
+                    opacity: 0.8,
+                }}
+            >
+                {quotes.length === 1 ? "Sitat" : "Sitater"}
+            </Link>  
+            <Box sx={{
+                mt: "-10px",
+                borderLeftStyle: "solid",
+                borderLeftWidth: "6px",
+                borderLeftColor: theme => theme.palette.divider,
+                paddingLeft: "15px",
+            }}>
+                <QuoteList 
+                    quotes={quotes}
+                    onItemChanged={onItemChanged}
+                />
+            </Box>
+        </div>
     )
 }
--- client/src/routes/home/Feed/FeedItems/Rumour.tsx
+++ client/src/routes/home/Feed/FeedItems/Rumour.tsx
@@ -1,6 +1,9 @@
+import { Box, Link, Stack } from "@mui/material"
 import { Rumour } from "common/interfaces"
 import { RumourFeedItem as RumourFeedItemType } from "common/types"
 import { RumourList } from "src/routes/rumour/RumourPage"
+import RumourIcon from '@mui/icons-material/Hearing'
+import { Link as RouterLink } from "react-router-dom"
 
 export function RumourFeedItem({ 
     data, 
@@ -16,11 +19,52 @@ export function RumourFeedItem({
     }) satisfies Rumour)
     
     return (
-        <div style={{ marginTop: "10px", marginBottom: "10px" }}>
-            <RumourList 
-                rumours={rumours}
-                onItemChanged={onItemChanged}
-                />
+        <div>
+            <Stack direction="row" 
+                gap="11px"            
+                sx={{
+                    marginLeft: "-13px",
+                    marginBottom: "5px",
+                }}
+            >
+                <RumourIcon sx={{
+                    opacity: 0.8,
+                }}/>
+                <Link
+                    color="Inherit"
+                    component={RouterLink}
+                    to={`/sitater`}
+                    underline="hover"
+                    sx={{
+                        fontWeight: "bold",
+                        fontSize: "small",
+                        opacity: 0.8,
+                    }}
+                >
+                    {rumours.length === 1 ? "Rykte" : "Rykter"}
+                </Link> 
+            </Stack>
+            <Box sx={{
+                borderLeftStyle: "solid",
+                borderLeftWidth: "6px",
+                borderLeftColor: theme => theme.palette.divider,
+                paddingLeft: "15px",
+            }}>
+                <div style={{
+                    fontWeight: "bold",
+                    fontSize: "small",
+                    opacity: 0.8,
+                    fontStyle: "italic",
+                    marginBottom: "-10px",
+                    marginLeft: "2px"
+                }}>
+                    Har du hørt at...
+                </div>
+                <RumourList 
+                    rumours={rumours}
+                    onItemChanged={onItemChanged}
+                    />
+            </Box>
         </div>
     )
 }
